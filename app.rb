8f638271c8cc4c8377334b65a3dad229913a87aa
Shana L Moore <shana.lavina.moore@gmail.com>
2015-12-29 15:24:03 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
+
 require_relative 'config/environment'
 
-class App < Sinatra::Base 
+class App < Sinatra::Base
 
   get '/' do 
     erb :user_input
   end
 
-  post '/piglatinize' do
-    @word = params[:word]
-    redirect "/piglatinize/#{@word}"
+  post '/piglatinize' do 
+    p = PigLatinizer.new
+    @piglatin = p.to_pig_latin(params[:user_phrase])
+    erb :piglatinize
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,32 +1,25 @@
 class PigLatinizer
-  attr_accessor :word
-
-  def initialize
-    @word = word
-  end
-  
-Alpha = ('a'..'z').to_a
-Vowels = %w[a e i o u]
-Consonants = Alpha - Vowels
 
 def piglatinize (word)
-  if word.size == 1
-    word
-  elsif Vowels.include?(word[0])
-    word + 'ay'
-  elsif Consonants.include?(word[0]) && 
-    Consonants.include?(word[1])
-    word[2..-1] + word[0..1] + 'ay'
-  elsif word[0..1] == "qu" 
-    word[2..-1]+"quay"
-  elsif word[0..2] == "squ"
-     word[3..-1]+"squay"
-  else Consonants.include?(word[0])
-    word[1..-1] + word[0..0] + 'ay'
+
+    exclude = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if exclude.include?(word)
+      word
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
   end
-end
 
   def to_pig_latin (sentence)
-    sentence.split.map(&method(:piglatinize)).join(' ')
+    sentence.split.map{|word| piglatinize(word)}.join(' ')
   end
 end
