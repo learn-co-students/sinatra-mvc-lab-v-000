4f58cfff8a3337a99a6a1ab2ce99dbc8cdcc06b3
frankNowinski <nowinski.frank@gmail.com>
2015-12-31 01:19:07 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+class PigLatinizer
+  VOWELS = %w[a e i o u]
+
+  def piglatinize(word)
+    return word if ["i", "me", "to", "too", "a", "an", "in", "and", "on"].include?(word)
+
+    word = word.gsub(".", "")
+    index = ""
+    word.split("").each_with_index do |c, i|
+      if VOWELS.include?(c)
+        index = i
+        break
+      end
+    end
+
+    starts_with_vowel?(word) || "#{word[index..-1]}#{word[0..(index-1)]}ay"
+  end
+
+  def starts_with_vowel?(word)
+    if VOWELS.include?(word[0])
+      "#{word}ay"
+    end
+  end
+
+  def to_pig_latin(sentence)
+    sentence.split(" ").collect{|word| piglatinize(word)}.join(" ")
+  end
+end
