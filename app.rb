ffb6275feff77228a2c6b54c72c4fe27a7d85236
Chris Aporta <christopher.aporta@gmail.com>
2016-01-17 22:59:26 -0500

    complete lab

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
-require_relative 'config/environment'
-
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    asephray = PigLatinizer.new
+    "#{asephray.to_pig_latin(params[:user_phrase])}."
+  end
+
 end
