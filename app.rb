4da42c8e2cbac470457ee53f59bc48d33cb24c72
coltonstaab1 <colton.staab@gmail.com>
2016-01-25 19:23:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get "/" do
+		erb :user_input
+	end
+
+	post "/piglatinize" do
+
+		piglatin = PigLatinizer.new
+		@transformed_phrase = piglatin.to_pig_latin(params[:user_phrase])
+		
+		erb :results
+	end	
+
 end
