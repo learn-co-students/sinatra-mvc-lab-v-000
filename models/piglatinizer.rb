bbe026c569d1602cbe1b1d455bfbcf2d5790d0a6
Dakota Martinez <dakotaleemusic@gmail.com>
2016-02-07 07:10:29 +0000

    adds support for words ending in punctuation

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,18 +4,40 @@ class PigLatinizer
     first_vowel = word.scan(/[aeiou]/).first
     split_array = word.split(//)
     if split_array.count > 1
+      # adds 'yay' to the end of the word if
+      # the word starts with a vowel
       if split_array.index(first_vowel) == 0
-        split_array << "yay"
+        if ends_with_punc?(split_array.last)
+          punctuation = split_array.pop
+          split_array << "yay"
+          split_array << punctuation
+        else
+          split_array << "yay"
+        end
         split_array.join
       else
-        last_syllable = split_array.shift(split_array.index(first_vowel))
-        split_array << last_syllable
-        split_array << "ay"
+        # makes sure punctuation stays at 
+        # the end of the word
+        if ends_with_punc?(split_array.last) 
+          last_syllable = split_array.shift(split_array.index(first_vowel))
+          punctuation = split_array.pop
+          split_array << last_syllable
+          split_array << "ay"
+          split_array << punctuation
+        else
+          last_syllable = split_array.shift(split_array.index(first_vowel))
+          split_array << last_syllable
+          split_array << "ay"
+        end
         split_array.join
       end
     else
       word
     end
+  end
+  
+  def ends_with_punc?(char)
+    char.scan(/\p{P}/).count > 0 ? true : false
   end
   
   def to_pig_latin(words)
