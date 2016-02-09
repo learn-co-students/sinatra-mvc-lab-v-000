282a6a64a7fb33bf0a58217635ded22a407131a1
Adam King <adam.king0126@gmail.com>
2016-02-09 17:36:44 -0600

    got a bug

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @piglatinizer = PigLatinizer.new
+    puts params
+    @phrase = @piglatinizer.piglatinize(params[:user_phrase])
+    erb :result
+  end
 end
