eddfd3cdb8f6d2298945bb94ad2ae72b5a47d2fa
Brad1974 <danieltbrad@gmail.com>
2016-02-17 20:14:42 -0800

    Done.

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do 
-    erb :index
-  end
-
-  post '/' do 
-    @piglatinized_words = PigLatinizer.new(params[:user_phrase])
     erb :user_input
   end
+
+  post '/piglatinize' do 
+    variable= PigLatinizer.new
+    @piglatinized_words = variable.to_pig_latin(params[:user_phrase])
+    erb :results
+  end
+
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,24 @@
+require 'pry'
 class PigLatinizer
 
   attr_accessor :word
 
-  def to_pig_latin(string)
+   def to_pig_latin(string)
     stripped = string.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
     word_array = stripped.split(" ")
     pig_array = word_array.collect do |word|
-      if word == "a"
-        "a"
-      elsif word == "and"
-        "and"
-      elsif word == "i"
-        "i"
-      else
         piglatinize(word)
-      end 
     end
     pig_array.join(" ")
   end
 
   def piglatinize(word)
-    reverse_word = word.split(/([aeiou].*)/).reverse.join+"ay"
+    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    if non_pig_latin_words.include?(word)
+      word
+    else
+      reverse_word = word.split(/([aeiou].*)/).reverse.join+"ay"
+    end
   end
 
 end
