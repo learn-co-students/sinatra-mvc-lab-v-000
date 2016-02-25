3dc9db73b04961d3f1a9b20ba84ed1a31934c9a2
Jacob <mailme711@gmail.com>
2016-02-24 17:15:44 -0800

    Revert "should be done"
    This reverts commit 342dd70d0aa171b5569b10b10fe817a78a8a391f.

--- app.rb
+++ app.rb
@@ -1,14 +1,4 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get "/" do
-    erb :user_input
-  end
-
-  post '/piglatinize' do
-    pl = PigLatinizer.new
-    @piglatin = pl.to_pig_latin(params[:user_phrase])
-    erb :piglatin
-  end
 end
