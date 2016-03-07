542837ebbc78e9e8b1fe41efd6a0136b0b9b5595
ed-karabinus <ejkarabin@gmail.com>
2016-03-07 01:36:46 -0600

    Finished Sinatra MVC Lab

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do 
+    pig_latinizer = PigLatinizer.new
+    @pig_latinized_text = pig_latinizer.to_pig_latin(params["user_phrase"])
+    erb :results
+  end
 end
