94ab93bc6b6a03e1bbe3023466558f4144b83ce7
kylegraydev <kylegraydev@gmail.com>
2016-03-15 22:57:26 -0700

    everything except model logic

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @the_text = Piglatinizer.new(params[:user_text])
+    erb :piglatinized
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,14 @@
+class Piglatinizer
+  attr_accessor :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  #split up the words
+    @text.split
+  #identify the first vowel
+
+  #put everything before the first vowel
+
+end
