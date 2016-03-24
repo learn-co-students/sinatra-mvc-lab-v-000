7d353df33b934e9087077fe084dc99ade5b4f520
rocketNeck <homestarflyer@gmail.com>
2016-03-24 19:58:19 -0400

    un-day

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def piglatinize(word)
+    vowels = ["a","e","i","o","u"]
+    first_vowel_index = word.chars.find_index{|char| vowels.include?(char.downcase)}
+    if first_vowel_index == 0
+      word + "way"
+    else
+      first_cons = word[0..first_vowel_index -1]
+      leftovers = word[first_vowel_index..-1]
+      leftovers + first_cons + "ay"
+    end
+  end
+
+  def to_pig_latin(sentence)
+    words = sentence.split(" ")
+    words.map{|w| piglatinize(w)}.join(" ")
+  end
+
+end
