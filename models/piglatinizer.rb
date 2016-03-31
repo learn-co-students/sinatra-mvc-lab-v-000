93abbaae44eb7f97a47784a7d77109e134d16a5c
smithWEBtek <brad@smithwebtek.com>
2016-03-31 11:14:43 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,51 @@
+class PigLatinizer
+
+  def to_pig_latin(sentence)
+    @newtext = []
+    text = sentence.split(" ")
+    text.each do |word|
+      piglatinize(word)
+      @newtext << @pigtext
+    end
+    @user_phrase = @newtext.join(" ")
+  end
+
+  def piglatinize(text)
+    if text[0].match(/\b[aeiouAEIOU]/)
+      vowel(text)
+    else
+      consonant(text)
+    end
+  end
+
+  def vowel(text)
+    @pigtext = "#{text}way"
+    @pigtext
+  end
+
+  def consonant(text)
+    if text[1].match(/\b[aeiouAEIOU]/)
+      ary = text.split("")
+      ary.shift
+      ary << text[0]
+      ary = ary.join("")
+      @pigtext = "#{ary}ay"
+      
+    elsif text[2].match(/\b[aeiouAEIOU]/)
+      ary = text.split("")
+      2.times do ary.shift end
+      ary << text[0..1]
+      ary = ary.join("")
+      @pigtext = "#{ary}ay"
+
+    else
+      puts "you made it to the 3-letter consonant else code"
+      ary = text.split("")
+      3.times do ary.shift end
+      ary << text[0..2]
+      ary = ary.join("")
+      @pigtext = "#{ary}ay"
+    end
+    @pigtext
+  end
+end
