dc32cc9f6d1dfa2f4a86720380bfca3fa3ed6c1c
Chris Haugen <haugenc@gmail.com>
2016-04-06 12:31:00 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,13 +5,21 @@ class PigLatinizer
   end
 
   def piglatinize(word)
+    new_word = ""
     case
     when word.downcase.match(/^[aeiou]/)
-      word + "yay"
-    when word.downcase.match(/^[^aeiou])
-      word.gsub(/(.)(.*)/,'\2\1ay')
-    else
-      "Not Guilty"
+      new_word = word + "way"
+    when word.downcase.match(/^[^aeiou]/)
+      new_word = word.downcase.gsub(/(^[^aeiou]+)(.*)/,'\2\1ay')
     end
+    word.downcase == word ? new_word : new_word.capitalize
+  end
+
+  def to_pig_latin(phrase)
+    new_phrase = Array.new
+    phrase.split().each do |word|
+      new_phrase << piglatinize(word)
+    end
+    new_phrase.join(" ")
   end
 end
