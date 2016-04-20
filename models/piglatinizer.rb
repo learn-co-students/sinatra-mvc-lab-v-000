3613cd4be75f202b9f3d419b0be2d164b475e02a
Vannak Chhay <vannak.chhay1@gmail.com>
2016-04-19 22:28:09 -0400

    done

---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+  attr_accessor :text
+
+  def initialize(text = nil)
+    @text = text
+  end
+
+  def piglatinize
+    words = @text.split(" ")
+    arr_words = []
+    words.map do |word|
+      if (word =~ /yY/) == 0
+        arr_words << word[1..-1] + 'y' + 'ay'
+      else
+        vowel_location = first_vowel(word)
+        arr_words << word[vowel_location..-1] + word[0..vowel_location-1] + 'ay'
+      end 
+    end
+    arr_words.join(" ")
+  end
+
+  def first_vowel(word)
+    word =~ /[aeoui]/
+  end
+end
