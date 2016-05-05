4a43ed4aef5ce68d3b4450c615bfe3e29fa3d924
joshnevius <joshnevius92@gmail.com>
2016-05-05 15:17:01 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,17 @@
+class PigLatinizer
+  
+  def piglatinize(word)
+    if /^[aeiou]/i =~ word
+      word + "way"
+    else
+      consonants = /^[^aeiou]*/.match(word)[0]
+      word[consonants.length..-1] + consonants + "ay"
+    end
+  end
+
+  def to_pig_latin(sentence)
+    sentence.split.map do |word| 
+      piglatinize(word) 
+    end.join(" ")
+  end
+end
