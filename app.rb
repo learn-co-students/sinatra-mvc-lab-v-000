4ececeff2de2e8d16d8e9275a0a6721a51d55821
thebrianemory <the@brianemory.com>
2016-05-08 22:41:40 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    piggie = PigLatinizer.new
+    @pig_latin = piggie.to_pig_latin(params[:user_phrase])
+
+    erb :results
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,50 @@
+class PigLatinizer
+
+  def split_phrase(phrase)
+    new_phrase = phrase.split(" ")
+    new_phrase
+  end
+
+  def to_pig_latin(phrase)
+    phrasing = split_phrase(phrase)
+    latinized = []
+    phrasing.each do |word|
+      latinized << piglatinize(word)
+    end
+    latinized.join(" ")
+  end
+
+  def piglatinize(word)
+    first_split = word.split(/([aeiouAEIOU].*)/)[0]
+    second_split = word.split(/([aeiouAEIOU].*)/)[1]
+    if first_split == ""
+      second_split + "way"
+    else
+      second_split + first_split + "ay"
+    end
+  end
+end
+
+
+
+# def to_pig_latin(phrase)
+#   @phrasing = split_phrase(phrase)
+#   piglatinize
+#   @latinized.join(" ")
+# end
+#
+# def piglatinize
+#   @latinized = []
+#   i = 0
+#   while i < @phrasing.count
+#     first_split = @phrasing[i].split(/([aeiou].*)/)[0]
+#     second_split = @phrasing[i].split(/([aeiou].*)/)[1]
+#     if first_split == ""
+#       @latinized << second_split + "way"
+#     else
+#       @latinized << second_split + first_split + "ay"
+#     end
+#     i += 1
+#   end
+# end
+# end
