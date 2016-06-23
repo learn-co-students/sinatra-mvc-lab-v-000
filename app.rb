b425b169749274b360ceddac01776d348ca1036b
wedesignstudios <deelden@gmail.com>
2016-06-23 10:45:24 -0500

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/pig_latinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @piggy = PigLatinizer.new
+    erb :piglatinize
+  end
+
 end
