1811d22dad2f5f74279af20a77135cfd9a749acf
IshmaelKhalid <ishmaelkhalid@icloud.com>
2016-06-29 14:32:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    erb :user_input
+  end
+
+  post "/piglatinize" do
+    original_text = params[:user_phrase]
+    if original_text.split(" ").size == 1
+      @piglatinized_text = PigLatinizer.new.piglatinize(original_text)
+    else
+      @piglatinized_text = PigLatinizer.new.to_pig_latin(original_text)
+    end
+    "#{@piglatinized_text}"
+  end
 end
