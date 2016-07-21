4e567b6acc7ab515dc2c3ea9b6442db6fe5227cc
Shmuwol <shmullywolfson@gmail.com>
2016-07-21 12:38:37 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,27 @@
 class PigLatinizer
 
-  def piglatinize(words)
-    
+  def piglatinize(word)
+    vowels = %w[a e i o u A E I O U]
+    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
 
+    @word = word
+
+    if vowels.include?(@word[0])
+      # "#{@word}ay"
+      @word
+    else
+      case @word
+      when condition
+
+      end
+
+    end
+
+    # @vowels = @word.scan(/[aeoui]/)
+    # require "pry" ; binding.pry
   end
+
+
+    # @word.scan(/[aeoui]/)
 
 end
