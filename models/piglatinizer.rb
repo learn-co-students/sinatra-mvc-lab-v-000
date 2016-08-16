7a4216c46927d7368f5c3e6bd7af59dac0e45a9d
Jason Casares <jasonc@csdrafting.com>
2016-08-16 16:09:46 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,20 @@
 class PigLatinizer
 
   def piglatinize(word)
-    vowels = %w{a e i o u}
-     word.gsub(/(\A|\s)\w+/) do |str|
-             str.strip!
-         while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
-             str += str[0]
-             str = str[1..-1]
-         end
-         str  = ' ' + str + 'ay'
-     end.strip
-   end
+    if (word[0] =~ /[aeiou]/i)
+      case word[-1]
+      when /[aeiou]/
+        word += "way"
+      when /y/
+        word += "way"
+      else
+        word += "way"
+      end
+    else
+      consonants = word.slice!(/^[^aeiou]*/)
+      word += consonants + "ay"
+    end
+  end
 
 
   def to_pig_latin(string)
