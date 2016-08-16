f815245a25e151d9f705b7be7da974f6cb9d7738
Jason Casares <jasonc@csdrafting.com>
2016-08-16 15:52:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
-    
-  post '/' do
-    params[:name]
-    erb :user_input
-  end
 
+  post '/piglatinize' do
+    p = PigLatinizer.new
+    @piglatin = p.to_pig_latin(params[:user_phrase])
+    erb :results
+  end
 
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,24 @@
-class PigLatinize
+class PigLatinizer
+
+  def piglatinize(word)
+    vowels = %w{a e i o u}
+     word.gsub(/(\A|\s)\w+/) do |str|
+             str.strip!
+         while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+             str += str[0]
+             str = str[1..-1]
+         end
+         str  = ' ' + str + 'ay'
+     end.strip
+   end
+
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
+  end
 
 end
+
+p = PigLatinizer.new
+
+puts p.piglatinize("pork")
