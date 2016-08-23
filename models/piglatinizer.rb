1262b875bb7d0fd3830543f44ed932ab610ade59
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-23 23:41:10 +0000

    Done

---
+++ models/piglatinizer.rb
@@ -0,0 +1,17 @@
+class PigLatinizer
+
+  def piglatinize(word)
+    if word[0].match(/[b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z]/)
+      before_first_vowel = (word.match(/\w[^aeiou]*/)).to_s
+      word.gsub((before_first_vowel),'' )+before_first_vowel+"ay"
+    else
+    	word + "way"
+    end
+
+  end
+
+  def to_pig_latin(words)
+    
+  end
+
+end
