dd8cd2c96f955f25d3e47239f67fb26b657046e7
ThisIsTyDell <milleb94@gmail.com>
2016-08-22 21:59:09 -0500

    PigLatinizer class to change words and phrases into pig latin

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  attr_accessor :phrase
+
+  def piglatinize(word)
+    
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    if vowels.include? word[0]
+      word << "way"
+    else
+      word << word.slice!(/^[^aeiou]*/i || "") + 'ay'
+    end
+  end
+
+  def to_pig_latin(phrase)
+    phrase.split.each do |word|
+      self.piglatinize(word)
+    end.join(" ")
+  end
+end
