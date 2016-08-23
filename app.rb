56191eda7d6cb6ca7f3aeb7bdf5e67fbaa8520ff
ThisIsTyDell <milleb94@gmail.com>
2016-08-22 22:49:05 -0500

    instantiate new Piglatinizer in post/piglatinize route and display result in browser

--- app.rb
+++ app.rb
@@ -7,5 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
+    translator = PigLatinizer.new
+    @piglatin = translator.to_pig_latin(params[:user_phrase])
+    erb :results
   end
 end
