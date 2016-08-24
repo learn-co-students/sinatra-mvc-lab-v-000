dae5c94f55b00e8565925df467db5e6a5b5e2eea
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-24 07:47:44 +0000

    Changed gsub to sub which solved the last spec test . Passed all 12 specs.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,10 +3,7 @@ class PigLatinizer
   def piglatinize(word)
     if word[0].downcase.match(/[b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z]/)
       before_first_vowel = (word.match(/\w[^AEIOUaeiou]*/)).to_s
-      word.gsub(before_first_vowel,'' ) + before_first_vowel + "ay"
-
-    elsif word[0].downcase.match(/[b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z]/) && word[-2..-1].downcase.scan(/ay/)
-      word.gsub(before_first_vowel,'' ) + before_first_vowel
+      word.sub(before_first_vowel,"" ) + before_first_vowel + "ay"
     else
     	word + "way"
     end
