8a9a2d918a10392d2f41f2ccb9e9061aff0aaaec
BriOD <briodonoghue@gmail.com>
2016-09-16 16:35:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
+# require_relative 'config/environment'
+#
+# class App < Sinatra::Base
+#   get '/' do
+#     erb :user_input
+#   end
+#
+#
+#   post '/piglatinize' do
+#     pl = PigLatinizer.new
+#     @piglatin = pl.to_pig_latin(params[:user_phrase])
+#     erb :results
+#   end
+# end
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    new_phrase = PigLatinizer.new
+    @piglatin = new_phrase.to_pig_latin(params[:user_phrase])
+    erb :results
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,26 @@
+
+class PigLatinizer
+
+  def piglatinize(word)
+    edge_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ['a','e','i','o','u']
+
+    if vowels.include?(word[0].downcase) || edge_words.include?(word.downcase)
+      word << "way"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
+  end
+
+  def to_pig_latin(words)
+    words.split.collect do |word|
+      piglatinize(word)
+    end.join(" ")
+  end
+
+end
