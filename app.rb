2a15625a368aee67aa02f2d6c08a345be316ef8f
Brad Watson <brad.watson.orlando@gmail.com>
2016-09-24 12:28:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    p = PigLatinizer.new
+    @latinized_text = p.to_pig_latin(params[:user_phrase])
+    erb :results
+  end
+
 end
