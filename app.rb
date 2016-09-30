549025fa2176fc284cced52cb52fc71f034bd094
Nana Adu-Krow <nadukrow@gmail.com>
2016-09-30 17:48:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pig_Latinizer = PigLatinizer.new
+    @phrase = pig_Latinizer.to_pig_latin(params[:user_phrase])
+    erb :pig_latin
+  end
 end
