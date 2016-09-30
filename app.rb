a7081ab8016c4fe91ecc8e7eac5be7fea3fddccb
Emmanuel Owusu <e_owusu@live.com>
2016-09-30 01:42:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get 'user_input' do
+
+    erb :user_input
+  end
+
+  get '/' do
+    erb :user_input
+  end
+
+
+  post '/' do
+    erb :user_input
+  end
+
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,14 @@
+class PigLatinizer
+  attr_accessor :piglatin
+
+  def initialize(words)
+    @words = words
+  end
+
+  def piglatinize()
+
+  end
+
+
+
+end
