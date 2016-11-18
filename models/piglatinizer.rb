6bbd52ce9db39ce05972a7adeb1212d84e8dea8e
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-11-18 09:37:40 -0500

    inatraSay isway igPay atinizedLay

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,11 +4,11 @@ class PigLatinizer
     alphabet = ('a'..'z').to_a
     vowels = %w[a e i o u]
     consonants = alphabet - vowels
-    letters = word.downcase.split("")
-    if vowels.include?(letters[0])
+    letters = word.split("")
+    if vowels.include?(letters[0].downcase)
       word + 'way'
     else
-      vowel_index = letters.find_index {|l| %w[a e i o u y].include?(l)}
+      vowel_index = letters.find_index {|l| %w[a e i o u y].include?(l.downcase)}
       translation = letters[vowel_index..-1]+letters[0..vowel_index-1]+['ay']
       translation.join
     end
