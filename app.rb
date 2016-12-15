1e91d8669de883dc6b196c987d0f5dc31d75cc9e
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-12-15 19:14:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+      erb :user_input
+    end
+
+    post '/piglatinize' do
+      @pig_latin = PigLatinizer.new
+      @user_phrase = params[:user_phrase]
+      erb :pig_latinized
+    end
 end
