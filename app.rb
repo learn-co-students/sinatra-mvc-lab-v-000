9b36f9944e3195cb07732859e4684a628a1b2424
Adam Lucia <lucia.adam@gmail.com>
2016-12-31 07:03:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @latin_text = PigLatinizer.new.to_pig_latin(params[:user_phrase])
+    erb :show_text
+  end
 end
