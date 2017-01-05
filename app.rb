bf2772a61a4beade76fc00b4c7c79f0575b954d5
Gordon Siu <gs2@GSMB2.local>
2017-01-05 14:33:21 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+    get "/" do
+        erb :user_input
+    end
+
+    post "/piglatinize" do
+        # binding.pry
+        # if params[user_phrase].scan(/[[:alpha:]]+/).count > 1
+        #     @result=PigLatinizer.new.to_pig_latin(params[user_phrase])
+        # else
+        @result=PigLatinizer.new.piglatinize(params[user_phrase])
+        # end
+        #erb :result
+
+        "#{@result}"
+    end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class PigLatinizer
+
+  def piglatinize(word)
+    #binding.pry
+    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if non_pig_latin_words.include?(word)
+      word
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
+  end
+
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
+  end
+
+end
+
+# p = PigLatinizer.new
+
+# puts p.piglatinize("pork")
