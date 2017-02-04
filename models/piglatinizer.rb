29c6bd75c12c8ea3a7e65825d48807d95ad4f8d6
github username <github email address>
2017-02-04 00:12:49 -0500

    completed

---
+++ models/piglatinizer.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class PigLatinizer
+
+  # def initialize(words)
+  #   @words = words
+  # end
+
+  def piglatinize(word)
+    # phrase = @words.downcase
+    # vowels = %w(a e i o u)
+    # phrase = words.split(' ')
+    consonants = ('a'..'z').to_a - %w(a e i o u)
+    piglatin = ''
+
+    # phrase.each do |w|
+
+    if !consonants.include?(word[0].downcase)
+      pl =  word + 'way'
+    elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase) && consonants.include?(word[2].downcase)
+      pl =  word[3..-1] + word[0..2] + 'ay'
+    elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
+      pl =  word[2..-1] + word[0..1] + 'ay'
+    elsif consonants.include?(word[0].downcase)
+      pl =  word[1..-1] + word[0] + 'ay'
+    else
+      pl =  word
+    end
+    piglatin += pl
+    piglatin.strip
+  end
+
+  def to_pig_latin(phrase)
+    phrase.split(' ').map{|w| self.piglatinize(w)}.join(' ')
+  end
+
+end
---
+++ views/results.erb
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pig Latinizer Results</title>
+  </head>
+  <body>
+    <h2>Your Piglatinized Phrase</h2>
+    <p><%= @result%></p>
+  </body>
+</html>
