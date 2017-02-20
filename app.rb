cb3aa19065ec2c0e5f3cb7ac039c5e384b4de362
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-20 15:11:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+
+  get '/' do 
+    erb :user_input
+  end 
+
+  post '/piglatinize' do 
+    kek = PigLatinizer.new
+    @text = kek.to_pig_latin(params[:user_phrase])
+    erb :piglatin
+  end 
+  
 end
