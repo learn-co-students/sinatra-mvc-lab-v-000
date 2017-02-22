ade861e9287ac5f46f72549885779233c22aec04
John Pfingst <jpfingst@outlook.com>
2017-02-22 02:29:06 +0000

    Sinatra MVC lab

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @new_string = PigLatinizer.new.piglatinize(params[:user_phrase])
+    erb :user_output
+  end
+
 end
