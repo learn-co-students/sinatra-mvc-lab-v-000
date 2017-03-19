d2be7d39f50a24cfda147c36d2f5d845e06af3a8
cquinones100 <cquinones100@gmail.com>
2017-03-18 20:58:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    # params[:user_phrase]
+    piglatin = PigLatinizer.new
+    # @text = piglatin.word(params[:user_phrase])
+    @pig_text = piglatin.to_pig_latin(params[:user_phrase])
+    # binding.pry
+    # # params[:user_phrase]
+    erb :pig_latinized
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class PigLatinizer
+
+  def word_splitter(text)
+    text.split(" ")
+  end
+
+  def to_pig_latin(text)
+    # binding.pry
+    word_splitter(text).collect {|word| piglatinize(word)}.join(" ")
+  end
+
+  def piglatinize(word)
+    if /[aeiou]/.match(word[0].downcase)
+      "#{word}way"
+    else
+      vowel_index = word.index(/[aeiou]/)
+      "#{word[vowel_index..-1]}#{word[0...vowel_index]}ay"
+    end
+  end
+
+end
