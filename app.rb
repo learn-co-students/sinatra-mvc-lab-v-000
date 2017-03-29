af3e0717a1a145eb201f47ca4cbeb10aa502c17c
Jenna Francis <jef399@nyu.edu>
2017-03-29 13:39:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @piglatinized_phrase = PigLatinizer.new
+    # binding.pry
+    erb :results
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+class PigLatinizer
+
+  def piglatinize(word)
+    if !!(word.downcase =~ /^[aeiou]/)
+      word + "way"
+    else
+      word.gsub(/\A[^aeiou]+/,"") + /\A[^aeiou]+/.match(word)[0] + "ay"
+    end
+
+  end
+
+  def to_pig_latin(phrase)
+    words = phrase.split(" ")
+    pig_array = words.collect do |word|
+      piglatinize(word)
+    end
+    pig_array.join(" ")
+  end
+end
