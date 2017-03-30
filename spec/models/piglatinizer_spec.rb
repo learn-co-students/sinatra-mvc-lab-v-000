bcdd197fea1cb4503e3ca7caa04cf372ecba8083
Daniel Lempesis <danlrcn@hotmail.com>
2017-03-30 20:17:17 +0000

    Done.

--- spec/models/piglatinizer_spec.rb
+++ spec/models/piglatinizer_spec.rb
@@ -1,26 +1,26 @@
-describe 'class PigLatinizer' do 
+describe 'class PigLatinizer' do
   let!(:words) { PigLatinizer.new }
 
 
-  it 'can create a new instance of a class' do 
+  it 'can create a new instance of a class' do
     expect(PigLatinizer.new).to be_an_instance_of(PigLatinizer)
   end
 
-  it 'piglatinizes an individual word' do 
-    expect(words.piglatinize("pork")).to eq("orkpay")
-    expect(words.piglatinize("I")).to eq("Iway")
-    expect(words.piglatinize("hello")).to eq("ellohay")
-    expect(words.piglatinize("please")).to eq("easeplay")
-    expect(words.piglatinize("tomorrow")).to eq("omorrowtay")
-    expect(words.piglatinize("until")).to eq("untilway")
-    expect(words.piglatinize("this")).to eq("isthay")
-    expect(words.piglatinize("Enumeration")).to eq("Enumerationway")
-    expect(words.piglatinize("spray")).to eq("ayspray")
-    expect(words.piglatinize("prays")).to eq("ayspray")
+  it 'piglatinizes an individual word' do
+    expect(words.piginize("pork")).to eq("orkpay")
+    expect(words.piginize("I")).to eq("Iway")
+    expect(words.piginize("hello")).to eq("ellohay")
+    expect(words.piginize("please")).to eq("easeplay")
+    expect(words.piginize("tomorrow")).to eq("omorrowtay")
+    expect(words.piginize("until")).to eq("untilway")
+    expect(words.piginize("this")).to eq("isthay")
+    expect(words.piginize("Enumeration")).to eq("Enumerationway")
+    expect(words.piginize("spray")).to eq("ayspray")
+    expect(words.piginize("prays")).to eq("ayspray")
   end
 
   it 'has a method splits the sentence to piglatinize each word' do
-    expect(words.to_pig_latin("i love programming")).to eq("iway ovelay ogrammingpray") 
+    expect(words.to_pig_latin("i love programming")).to eq("iway ovelay ogrammingpray")
   end
-  
+
 end
