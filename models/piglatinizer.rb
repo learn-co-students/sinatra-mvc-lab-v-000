5e50ca1d9ad2975791ccbc2b85175b30b218acb2
Seth <itz.saga@gmail.com>
2017-04-05 22:14:39 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+class PigLatinizer
+
+  def piglatinize(word)
+
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if vowels.include? word[0].downcase
+      word << "way"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
+  end
+
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
+  end
+
+end
