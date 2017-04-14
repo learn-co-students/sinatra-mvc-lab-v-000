c0f0cac993e55c6de7f68656bbdfc81eb792b1a3
Trai Compton <tlcompton2546@outlook.com>
2017-04-14 22:01:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    phrase = params[:phrase]
+    pl = PigLatinizer.new
+    @piglatin = pl.to_pig_latin(phrase)
+    erb :result
+  end
 end
