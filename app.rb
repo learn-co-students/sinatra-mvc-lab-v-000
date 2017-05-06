5756b16143319d9a243743d87390b3358ccfc546
lucketta <lucketta@umich.edu>
2017-05-05 22:03:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  get '/piglatinize' do
+
+  end
+
+  post '/piglatinize' do
+    @users_phrase = PigLatinizer.new
+    erb :piglatinize
+  end
 end
