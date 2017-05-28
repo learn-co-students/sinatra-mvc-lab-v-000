6f43316bbbf8b236adc52029454032924a8a0f9c
Emmanuel Owusu <e_owusu@live.com>
2017-05-28 04:15:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
 
-  get 'user_input' do
-
-    erb :user_input
-  end
 
   get '/' do
     erb :user_input
   end
 
 
-  post '/' do
-    erb :user_input
+  post '/piglatinize' do
+    pig_latin = PigLatinize.new
+    @piglatin = pig_latin.to_pig_latin(params[:user_phrase])
+    erb :results
   end
 
 
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,33 @@
 class PigLatinizer
-  attr_accessor :piglatin
+  attr_accessor
 
-  def initialize(words)
-    @words = words
-  end
-
-  def piglatinize()
+  def initialize()
 
   end
 
+  def piglatinize(text)
+    all_letters = [('a'..'z') || (/[^A-Z]*/)].to_a
+    vowels= %w([a e i o u] || [A E I O U])
+    consonants = all_letters - vowels
 
+    if vowels.include?(text[0])
+      text + 'way'
+    elsif consonants.include?(text[0]) &&
+          consonants.include?(text[1])
+           text[2..-1] + text[0..1] + 'ay'
+    elsif text[0..1] == "qu"
+         text[2..-1]+"quay"
+    elsif text[0..2] == "squ"
+          text[3..-1]+"squay"
+    else vowels.include?(text[0])
+         text[1..-1] + text[0] + 'ay'
+    end
+  end
+
+  def to_pig_latin(text)
+   split_text = text.split(",").to_a
+   phrase = piglatinize(split_text)
+   new_array = phrase.split(",")
+  end
 
 end
