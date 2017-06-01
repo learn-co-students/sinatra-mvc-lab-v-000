58e9e14286653459f71a97198828fc1814ddeffd
blinded93 <blinded93@gmail.com>
2017-06-01 12:26:45 -0700

    Done.

--- app.rb
+++ app.rb
@@ -5,10 +5,11 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
-  
+
   post '/piglatinize' do
     @user_phrase = params[:user_phrase]
     @pig_latin = PigLatinizer.new
     erb :piglatinize
   end
+  
 end
