804ddda524e92b5288b5e39deac7fe228731bc2c
James Dolan <jwdolan820@gmail.com>
2017-06-26 07:15:39 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,31 @@
+class PigLatinizer
+
+  def piglatinize(word)
+
+    non_pig_latin_words = ["i", "I", "me", "to", "too", "a", "an", "in", "and", "on", "it", "Once", "upon", "along", "alone", "eighty", "old", "until", "Enumeration"]
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if non_pig_latin_words.include?(word)
+      word << "way"
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
+  end
+
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
+  end
+
+end
+
+p = PigLatinizer.new
+
+puts p.piglatinize("pork")
