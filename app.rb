22ed2ca5eb4e1a53159e9025dcf2283e5eaacaf5
Scott Wakeman <swakeman@ca.rr.com>
2017-07-06 18:23:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    @piglatin_text = PigLatinizer.new(params[:user_text])
+    erb :results
+  end
 end
