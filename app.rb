7c75e66b4d0840c447d1b4ec1cc48db5a5ec81b7
emhopkins <epoletto@lstreetc.com>
2017-07-16 23:53:19 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+	get '/' do
+	  erb :user_input
+	end
+
+	post '/piglatinize' do
+	  phrase = PigLatinizer.new
+	  @piglatinized_text = phrase.piglatinize(params[:user_phrase])
+	  erb :piglatinize
+	end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,44 @@
+class PigLatinizer
+	@words_array = []
+
+	def split_into_words_array(phrase)
+		@words_array = phrase.split(" ")
+	end
+
+	def to_pig_latin(phrase)
+		#tests require separate method, but this can be handled in the same method
+		piglatinize(phrase)
+	end
+
+	def piglatinize(phrase)
+		@phrase = phrase
+		split_into_words_array(@phrase)
+		pig_latinized_text = ""
+		pig_latinized_word = ""
+		pig_latinized_array = []
+		@words_array.each do |word|
+			if starts_with_vowel?(word)
+				pig_latinized_word = word + "way"
+			else
+				pig_latinized_word = translate_consonant_start(word, number_of_starting_consonants(word)) + "ay"
+			end
+			pig_latinized_array << pig_latinized_word
+		end
+		pig_latinized_array.join(' ')
+	end
+
+	def starts_with_vowel?(word)
+		word.downcase.scan(/\A[aeiou]/).size > 0
+	end
+
+	def number_of_starting_consonants(word)
+		word.downcase.scan(/\A[bcdfghjklmnpqrstvwxyz]*/).join('').length
+	end
+
+	def translate_consonant_start(word, number)
+		characters_array = []
+		characters_array = word.split('')
+		characters_array = characters_array.rotate(number)
+		characters_array.join('')
+	end
+end
