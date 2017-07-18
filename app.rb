1c67fdb05bef2d679ec1e9e563fcc1c4a686e68d
hyoyou <heatherhyou@gmail.com>
2017-07-17 23:16:05 -0400

    update test to take post '/'

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinize' do
+  post '/' do
     phrase = PigLatinizer.new
     @user_phrase = phrase.to_pig_latin(params[:user_phrase])
     erb :piglatinize
