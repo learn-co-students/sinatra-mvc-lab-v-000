a5afaaa21ee03223884e83bc133ea9abc1274ffa
Zain <zain@halai.us>
2017-07-22 00:01:01 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,17 @@
+class PigLatinizer
+
+  def piglatinize(word)
+    ( word[0].match(/[aeiou]/i) ) ? word + 'way' : consonant_word(word)
+  end
+
+  def consonant_word(word)
+    first_vowel_index = word.index(/[aeiou]/) || 0
+   
+    word[first_vowel_index, word.length] + word[0, first_vowel_index] + 'ay'
+  end
+
+  def to_pig_latin(sentence)
+    sentence.split(' ').map { |word|  piglatinize(word) }.join(' ')
+  end
+
+end
