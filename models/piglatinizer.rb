0b77ba8c557a33bf895bf349652d784789c91716
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-07-25 04:38:20 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,16 @@
+class PigLatinizer
+  def piglatinize(word)
+    if word.chars.first.match(/[^aeiouAEIOU]/)
+      array = word.split("")
+      first_vowel = array.find{|a| a.match(/[aeiou]/) }
+      string = array.drop(array.index(first_vowel)).join
+      consonent = array[0..array.index(first_vowel)-1].join
+      new_word = "#{string}#{consonent}ay"
+    else
+       new_word = "#{word}way"
+    end
+  end
+
+
+
+end
