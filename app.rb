0b33de0594cf0ab47258bf15bc1910d02b54204d
alexisadorn <alexisadorn@gmail.com>
2017-08-16 20:16:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
     user_word = params["user_phrase"]
     word = PigLatinizer.new
-    @pig_latinized_word = word.piglatinize(user_word)
+    @pig_latinized_word = word.to_pig_latin(user_word)
 
     erb :pit_latin
   end
