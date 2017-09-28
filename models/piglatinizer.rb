2c5ffa24f9a76e127c9385304a6880020c7304b6
github username <github email address>
2017-09-28 00:52:18 -0700

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -23,15 +23,36 @@ class PigLatinizer
   end
 
   def to_pig_latin(words)
-    words_arr = words.split(" ")
-    piggyd = words_arr.map do |word|
-      piglatinize(word)
-    end
-    piggyd.join(" ")
+    # words_arr = words.split(" ")
+    # piggyd = words_arr.map do |word|
+    #   piglatinize(word)
+    # end
+    # piggyd.join(" ")
+    words.split(" ").map{|word| piglatinize(word)}.join(" ")
   end
 
 
 end #ends class
+
+# def piglatinize(word)
+#
+#   non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+#   vowels = ["a", "e", "i", "o", "u"]
+#
+#   if non_pig_latin_words.include?(word)
+#     word
+#   elsif vowels.include? word[0]
+#     word << "ay"
+#   else
+#     consonants = ""
+#     while !vowels.include?(word[0])
+#       consonants << word[0]
+#       word = word.split("")[1..-1].join
+#     end
+#     word + consonants + 'ay'
+#   end
+# end
+
 
 =begin
   check if the word starts with a vowel
