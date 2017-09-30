e59291f2cbf25e298bb1acd7b57bac5426558373
Amanda Wehrman <arwehrman@gmail.com>
2017-09-30 18:56:04 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,34 @@
+class PigLatinizer
+
+  VOWELS = ['a','e','i','o','u','A','E','I','O','U']
+
+  def piglatinize(word)
+    output = ""
+    vowelFound = false
+
+    if word.start_with?('a','e','i','o','u','A','E','I','O','U')
+      output  = word + 'way'
+    else
+      output = word
+      word.each_char do |chr|
+        if (!vowelFound && !(VOWELS.include? chr))
+          output = output + output.slice!(0)
+        else
+          vowelFound = true
+        end
+      end
+      output += 'ay'
+    end
+    output
+  end
+
+  def to_pig_latin(words)
+    wordArray = words.split
+    output = ""
+    wordArray.each do |word|
+      output += self.piglatinize(word) + " "
+    end
+    output.chop
+  end
+
+end
