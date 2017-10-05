45a99c54b91cca50ba1aee46c81396b3090f286f
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-10-04 21:04:58 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @input = params[:user_phrase]
+    @new_piglatin = PigLatinizer.new(@input)
+    erb :piglatinize
+  end
+
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,33 +1,23 @@
 class PigLatinizer
   attr_reader :text
 
-  def piglatinize(word)
-    vowels = ["a", "e", "i", "o", "u"]
-    invalid_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on", "until"]
+  @@vowels = ["a", "e", "i", "o", "u"]
+  @@invalid_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on", "until"]
 
-    if invalid_words.include?(word.downcase)
-      word << "way"
-    elsif !vowels.include? word[0].downcase
-      new_array << word[0]
-      new_word = "#{word}"[1..-1]
-      return new_word << new_array.join + "ay"
+  def piglatinize(word)
+    if @@invalid_words.include?(word.downcase)
+      word << 'way'
+    else
+      if @@vowels.include?(word[0].downcase)
+        word << 'way'
+      else
+        split_array = word.split /([aeiou].*)/
+        consonants = split_array[0].to_s
+        base_word = split_array[1].to_s
+        updated_word = base_word << consonants + 'ay'
+      end
     end
 
-    # if invalid_words.include?(word.downcase)
-    #   word << "way"
-    # elsif !vowels.include? word[0].downcase
-    #   new_array = []
-    #   new_word = word
-    #   while !vowels.include? new_word[0].downcase
-    #     new_array << word[0]
-    #     new_word = "#{word}"[1..-1]
-    #     if vowels.include? new_word[0].downcase
-    #       return new_word << new_array.join + "ay"
-    #     end
-    #   end
-    # elsif vowels.include? word[0].downcase
-    #    word << "way"
-    # else
   end
 
   def to_pig_latin(sentence)
