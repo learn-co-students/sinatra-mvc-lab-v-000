c94ebf3dfc66d892a6e76bad55a44edf90f71c81
Jameson Bass <jamesonbass@gmail.com>
2017-10-06 00:29:38 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,38 @@
+class PigLatinizer
+  attr_reader :phrase
+
+  def piglatinize(word)
+    modify_word(word)
+  end
+
+  def to_pig_latin(sentence)
+    phrase = sentence.split(" ")
+    return_phrase = []
+
+    phrase.each do |word|
+      updated_word = modify_word(word)
+      return_phrase << updated_word
+    end
+
+    return_phrase.join(' ')
+  end
+
+
+  def modify_word(word)
+
+    vowels = ['a','e','i','o','u','A','E','I','O','U']
+
+    if vowels.include?(word[0])
+      word += 'way'
+    elsif !vowels.include?(word[0]) && vowels.include?(word[1])
+      word = word.split('').rotate.join('') + "ay"
+    else
+      vowel_index = word.split('').index{|letter| vowels.include?(letter)}
+      word = word.split('')
+      vowel_index.times {|i| word = word.rotate}
+      word = word.join('') + "ay"
+    end
+
+  end
+
+end
