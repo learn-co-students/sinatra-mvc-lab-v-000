3e2b186d6aaba430763239b38575d8f9ac1ad0a7
chrisjamesr <chrisjames.r@gmail.com>
2017-10-22 13:21:15 -0400

    correct to pig latin method name in route action

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinizer' do
+  post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.sentence_to_pl(params[:user_phrase])
+    @piglatin = pl.to_pig_latin(params[:user_phrase])
     erb :results
   end
 
