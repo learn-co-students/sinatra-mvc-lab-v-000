94317841925c55ecd4d14c617ee1e50e773266e4
GusVilla303 <gusvilla303@gmail.com>
2017-10-24 14:22:57 -0600

    Add get and post actions in controllers. Add piglatinize view

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    piglatinizer = PigLatinizer.new
+    @translated_word = piglatinizer.to_pig_latin(params[:user_phrase])
+
+    erb :piglatinize
+  end
 end
