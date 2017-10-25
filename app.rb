1bf285cd9e3c5ec7a030994ba5d7326a1f97764f
mandlerm <amaya.melissa@gmail.com>
2017-10-25 17:56:46 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  # post '/' do
+  #   @piglatinized = PigLatinizer.new
+  #   erb :latinized
+  # end
+
+  post '/piglatinize' do
+    @piglatinized = PigLatinizer.new
+    erb :latinized
+  end
 end
