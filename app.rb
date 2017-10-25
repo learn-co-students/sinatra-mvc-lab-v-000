b6710296aa0a3b2acae9d40abdaef75545ed6417
Richard Steinbach <steinbach.dick@gmail.com>
2017-10-25 15:08:24 -0700

    sinatra-mvc-lab-v-000

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
-
+require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+#  post '/user_input' do
+#    @piglatinized = PigLatinizer.new
+#    @latin = params[:user_phrase]
+#    erb :results
+#  end
+
+  post '/piglatinize' do
+    @piglatinized = PigLatinizer.new
+    @latin = params[:user_phrase]
+    erb :results
+  end
+
 end
