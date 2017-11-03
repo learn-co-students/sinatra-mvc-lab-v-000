cec40d29b46561a356319b53918f42d67347d70f
Denis Bondin <dsbondin@gmail.com>
2017-11-03 01:56:36 +0000

    ...

--- app.rb
+++ app.rb
@@ -5,8 +5,11 @@ require_relative 'models/piglatinizer'
 class App < Sinatra::Base
 
   get '/' do
-    @user_phrase = params[:phrase]
     erb :user_input
+  end
+
+  post "/piglatinize" do
+    "<center><h3>#{PigLatinizer.new.to_pig_latin(params[:user_phrase])}</h3></center>"
   end
 
 end
