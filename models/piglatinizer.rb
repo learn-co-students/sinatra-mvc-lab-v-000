73f3f37b53c4027f44fa5cb6a780e5f11c7bf260
Alexander Friedland <friedlandaj@gmail.com>
2017-11-04 20:50:34 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,8 +4,21 @@ class PigLatinizer
     if word.downcase.start_with?('a', 'e', 'i', 'o', 'u')
       word + "way"
     elsif word[0..2] =~ /[^aeiou]{3}/
-      
+      word[3..word.length] + word[0..2] + "ay"
+    elsif word[0..1] =~ /[^aeiou]{2}/
+      word[2..word.length] + word[0..1] + "ay"
+    else
+      word[1..word.length] + word[0] + "ay"
     end
+  end
+
+  def to_pig_latin(input)
+    word_array = input.split(" ")
+    arr = []
+    word_array.each {|word|
+      arr << piglatinize(word)
+    }
+    arr.join(" ")
   end
 
 
