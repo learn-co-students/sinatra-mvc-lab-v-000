0943045937ebf57b3c2ea8723d434818afd3af31
Adam Tropp <adt6261@gmail.com>
2017-11-15 21:46:40 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,26 +2,27 @@ require 'pry'
 class PigLatinizer
 
   def piglatinize(word)
-    if !["a","e","i","o","u"].include?(word[0])
-      word.split("")
-      for letter in word
-        binding.pry
-      until ["a","e","i","o","u"].include?(l)
-        word << l
-        word.slice!(i)
+    if ["A","E","I","O","U", "a", "e", "i","o","u"].include?(word[0])
+      word << "way"
+    else
+      word = word.split("")
+      word.each_with_index do |l, i|
+        if ["a","e","i","o","u"].include?(l) && !["a","e","i","o","u"].include?(word[0..(i-1)])
+          word = word.join("")
+          consonants_before_vowel = word[0..(i-1)]
+          word.slice!(consonants_before_vowel)
+          word << consonants_before_vowel
+          break
+        end
       end
       word << "ay"
-    else
-      word << "way"
     end
   end
 
-  def to_pig_latin
-    @string.each do |word|
-      word.each do |letter|
-        word.delete(letter[0])
-        word << "ay"
-      end
+  def to_pig_latin(sentence)
+    pig_sentence = sentence.split(" ").map do |word|
+      piglatinize(word)
     end
+    pig_sentence.join(" ")
   end
 end
