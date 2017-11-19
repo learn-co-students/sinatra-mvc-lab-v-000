58b5926d8d4bc6a252549e55655a626b94f31557
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-19 00:31:09 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,48 +1,4 @@
-# require_relative './config/environment'
-
-class PigLatinizer 
-	# attr_reader :string
-
-
-# 	def piglatinize(string)
-# 		words = []
-# 		words = string.split(" ")
-# 		if is_more_than_one_word?(words)
-# 			#
-# 			words.map do |w|
-# 				#binding.pry
-# 				change(w)
-# 			end 
-# 			words.join(" ")
-# 		else
-# 			change(string)
-# 		end	
-# 	end 
-
-
-# 	def change(word)
-		
-# 	  if word =~ (/\A[aeiou]/i)
-# 	    word = word.split("")[1..-1].join + 'way'
-# 	  elsif word =~ (/\A[^aeiou]/i)
-	
-# 	    match_first_letter = /\A[^aeiou]/i.match(word)
-# 	    @result = match_first_letter.post_match + match_first_letter.to_s + 'ay'
-
-# 	  end
-	 
-# 	end
-
-
-# 	def is_more_than_one_word?(string)
-	
-# 		if string.size == 1
-# 			false
-# 		else
-# 			true
-# 		end
-# 	end
-#end  
+class PigLatinizer
 
   def piglatinize(word)
 
@@ -68,8 +24,8 @@ class PigLatinizer
     string.split.collect{|word| piglatinize(word)}.join(" ")
   end
 
-end 
+end
 
-
-#issue: will need to add one for phrases. 
-
+# p = PigLatinizer.new
+#
+# puts p.piglatinize("pork")
