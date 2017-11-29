d5ada3151bbe64e1e2ce56142d1dedba00016dc8
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-11-29 05:06:14 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,21 +2,21 @@ class PigLatinizer
 
   attr_reader :text
 
-  def piglatinize(text)
-    vowels = "/a|e|i|o|u|A|E|I|O|U/"
-    consonants = "/b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z|B|C|D|F|G|H|J|K|L|M|N|P|Q|R|S|T|V|W|X|Y|Z/"
+  def piglatinize(word)
+    vowels = "[a,e,i,o,u,A,E,I,O,U]"
+    consonants = "[b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z,B,C,D,F,G,H,J,K,L,M,N,P,Q,R,S,T,V,W,X,Y,Z]"
 
-    if text[0].match(vowels)
-      text << "way"
+    if word[0].match(vowels)
+      word << "way"
     else
       new_word = ""
-      while text.chars[0].match(consonants)
-        new_word << text.chars[0]
-        text = text[1..-1]
+      while word[0].match(consonants)
+        new_word << word.chars[0]
+        word = word[1..-1]
       end
-      text << new_word + "ay"
+      word << new_word + "ay"
     end
-    text
+    word
   end
 
   def to_pig_latin(text)
