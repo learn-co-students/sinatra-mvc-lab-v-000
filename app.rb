9ec6633b35e64c3cb4de9e99b3705c95effd74ea
Alex <github email address>
2017-12-05 20:40:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
+#controller
 require_relative 'config/environment'
+#for the controller to have access to the model, it needs to be require_relative at the top
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+  post '/piglatinize' do
+    @words = PigLatinizer.new.to_pig_latin(params[:user_phrase])
+    erb :piglatinize
+  end
+
 end
+
+
+#Build a form to take user input in user_input.erb. Show that form using a GET request at /.
+#Create a POST method in your controller (app.rb) to receive your form's params.
+#Build a PigLatinize model (in your models directory) that converts a string into pig latin.
+#In your application controller, create an instance of the PigLatinize class to convert your user input to Pig Latin.
+#Use ERB within a new view to display the final pig latin string to the user.
---
+++ models/piglatinizer.rb
@@ -0,0 +1,31 @@
+
+class PigLatinizer
+	attr_accessor :words
+
+
+	def piglatinize(word)
+		vowels = ["a", "e", "i", "o", "u", "y"]
+		if vowels.include?(word[0].downcase)
+			word + "way"
+		elsif vowels.include?(word[1].downcase)
+			array = word.scan(/\w/)
+			array << array[0] + "ay"
+			array.shift
+			array.join
+		elsif vowels.include?(word[2].downcase)
+			array = word.scan(/\w/)
+			array << array[0] + array[1] + "ay"
+			array.shift(2)
+			array.join
+		elsif vowels.include?(word[3].downcase)
+			array = word.scan(/\w/)
+			array << array[0] + array[1] + array[2] + "ay"
+			array.shift(3)
+			array.join
+    	end
+	end
+
+	def to_pig_latin(phrase)
+		phrase.split.collect{|word| piglatinize(word)}.join(" ")
+	end
+end
