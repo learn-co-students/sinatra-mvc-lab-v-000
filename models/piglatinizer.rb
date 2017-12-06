1a2cd0c97f5acf24f8057241b5f10b9907acd0da
americanmouths <shiverr@gmail.com>
2017-12-05 18:16:53 -0800

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def initialize
+  end
+
+  def piglatinize(word)
+    if /[aeiou]/ === word[0].downcase
+      "#{word}way"
+    else
+      first = word.split(/[aeiou]/).first
+      word = word.sub(first,"")
+      "#{word}#{first}ay"
+    end
+  end
+
+  def to_pig_latin(string)
+    string.split.map {|word| piglatinize(word)}.join(" ")
+  end
+
+end
