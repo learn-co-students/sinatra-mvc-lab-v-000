0a26ec4f1d5dd54c4d97185e7e9e9c1c8d4545b2
Alex <github email address>
2017-12-05 21:25:13 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,23 +1,22 @@
-#controller
+#app.rb serves as the controller for the application
+
 require_relative 'config/environment'
-#for the controller to have access to the model, it needs to be require_relative at the top
+
+#because the controller uses the class PigLatinizer from /models/PigLatinize.rb, we have to require taht file here
 require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
 
-  get '/' do
-    erb :user_input
-  end
-  post '/piglatinize' do
-    @words = PigLatinizer.new.to_pig_latin(params[:user_phrase])
-    erb :piglatinize
-  end
+#when the server recieves a get request for the homepage'/' it will show the user_input.erb file located in views
+	get '/' do
+		erb :user_input
+	end
+
+#in the user_input.erb file in the views directory is a form, this form has a textare sends a post request to piglatinize
+	post '/piglatinize' do
+		@words = PigLatinizer.new.to_pig_latin(params[:user_phrase])    #user_phrase derives from form's textarea located in the user_input.erb file in the views directory , @words is also the attr_accessor for the PigLatinizer class
+    # @words will be visible in our piglatinize.erb file because it is stipulated below
+		erb :piglatinize
+	end
 
 end
-
-
-#Build a form to take user input in user_input.erb. Show that form using a GET request at /.
-#Create a POST method in your controller (app.rb) to receive your form's params.
-#Build a PigLatinize model (in your models directory) that converts a string into pig latin.
-#In your application controller, create an instance of the PigLatinize class to convert your user input to Pig Latin.
-#Use ERB within a new view to display the final pig latin string to the user.
