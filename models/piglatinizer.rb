80f933cc4ac6f830730c561d337f408f0b252036
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-15 03:02:02 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,14 +6,15 @@ class PigLatinizer
       if word[0] =~ /[aeiou]/
         word << "way"
       else
-        word.map do |letter|
+        split_word = word.split("")
+        split_word.map do |letter|
           consonants = []
           index = 0
           until letter =~ /[aeiou]/
             consonants << letter
             index += 1
           end
-          piglatin_array = word[index..-1]
+          piglatin_array = split_word[index..-1]
           piglatin_array << consonants
           piglatin_array << "ay"
         end
