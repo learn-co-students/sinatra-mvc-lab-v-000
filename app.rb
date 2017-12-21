bdfaefc2ca13c7c764bff1c712b831c74c5c46f3
Lara Sanders <larasanders@gmail.com>
2017-12-21 15:38:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
-
+require_relative 'models/piglatinizer'
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    piglatined = PigLatinizer.new
+    @piglatinize = piglatined.to_pig_latin(params[:user_input])
+
+    erb :Piglatinizer
+
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+  attr_reader :text
+
+
+def piglatinize(words)
+  if words[0].match(/a|e|i|o|u|A|E|I|O|U/)
+            words = words << "way"
+  elsif words[0].match(/[bcdfghjklmnpqrstvwxyz]/) && words[1].match(/[bcdfghjklmnpqrstvwxyz]/)&& words[2].match(/[bcdfghjklmnpqrstvwxyz]/)
+      words = words[3..-1]<<words[0..2]
+      words << "ay"
+  elsif words[0].match(/[bcdfghjklmnpqrstvwxyz]/) && words[1].match(/[bcdfghjklmnpqrstvwxyz]/)
+    words = words[2..-1]<<words[0..1]
+    words << "ay"
+  else
+    words = words[1..-1]<<words[0]
+    words << "ay"
+  end
+  words
+end
+
+def to_pig_latin(words)
+  words.split.collect{|word| piglatinize(word)}.join(" ")
+
+end
+end
