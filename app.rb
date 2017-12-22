fdc4e0255a0b0fba6cb65a6299604a71b5139fc6
will-sauer-learn <will@heueso.com>
2017-12-22 15:50:26 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  post '/piglatinize' do
+    pig_it = PigLatinizer.new
+    @results = pig_it.piglatinize(params[:user_phrase])
+    erb :results
+  end
+
+  get '/' do
+    erb :user_input
+  end
+
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,74 +1,40 @@
 class PigLatinizer
+    attr_accessor :raw_text
 
-#----------------------------------------------------------------
-#macros / attrs / etc.
-attr_accessor :raw_text
-
-def initialize (raw_text)
-@raw_text = raw_text
-end
-
-CONSONANTS = "bcdfghjklmnpqrstvwxyz"
-VOWELS = "aeoui"
-
-#----------------------------------------------------------------
-#instance
-
-def pig_the_string
-    binding.pry
-    pigged_string = ""
-    word_place = 1
-    self.words.each {|word| 
-                      pigged_string << pig_the_word(word)
-                      
-                      #just adds a space between all but last word
-                      if word_place < self.words.count
-                      pigged_string << " "
-                      end
-                      #then increments counter 
-                      word_place += 1
-                    }
-pigged_string
-end
-
-
-def pig_the_word (word)
-    
-    if begins_with_vowel? (word)
-        pigged_word = word << "ay"
-    else
-        pigged_word = consonant_swap(begins_with_how_many_consonants?(word), word) << "ay"
+    def initialize
     end
 
-pigged_word
-end
+    def piglatinize(raw_text)
+        pigged_string = raw_text.split.map{|word| pig_the_word(word)}
+        pigged_string.join(" ")
+    end
+
+    def to_pig_latin(raw_text)
+        piglatinize(raw_text)
+    end
 
 
-def words
-self.raw_text.split (" ")
-end
+    def pig_the_word (word)
+        if begins_with_vowel?(word)
+          
+          word = word + "way"
+        else
+          add_to_back = ""
+          while !begins_with_vowel?(word)
+            
+            add_to_back << word[0]
+            word = word[1..-1]
+            
+          end
+          
+          word + add_to_back + "ay"
+        end
+    
+    end
 
-def begins_with_vowel?(word)
-VOWELS.include?(word.chars[0])
-end
-
-def begins_with_how_many_consonants? (word)
-word.scan(/\A[bcdfghjklmnpqrstvwxyz]+/).first.length
-end
-
-def consonant_swap (length, word)
-"#{word[length,word.length]}#{word[0,length]}"
-end
-
-#----------------------------------------------------------------
-#class
-
-#----------------------------------------------------------------
-# private
-
-
-
-#--eoc
+    def begins_with_vowel?(word)
+        ["a","e","o","u","i"].include?(word.chars[0].downcase)
+    end
 end
 
 
