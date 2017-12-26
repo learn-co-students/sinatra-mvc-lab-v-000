bc9ab280ddd3c2803be2f44951eba943260dc047
Susan Fowler <infopro94@gmail.com>
2017-12-26 20:12:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,14 +11,8 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.piglatinize(params[:user_phrase])
+    @piglatin = pl.to_pig_latin(params[:user_phrase])
     erb :results
   end
-
-  # post '/piglatinize' do
-  #   binding.pry
-  #   @piglatin = Piglatinizer.new(params[:user_phrase])
-  #   erb :results
-  # end
 
 end
