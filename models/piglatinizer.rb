c3c9a798b2d54f9403457f0b569b9a9309bd96cd
Emily Howard <howard.emilya@gmail.com>
2018-01-02 23:47:56 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class PigLatinize
+  def PigLatinize(word)
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if vowels.include?(word[0].downcase)
+      word = word << "way"
+    else
+      otherword = ""
+      while !vowels.include?(word[0].downcase)
+        otherword << word[0]
+        word = word.spilt("")[1..-1].join
+      end
+      word + otherword + "ay"
+    end
+  end
+
+  def pig_string(string)
+    string.split.collect{|word| PigLatinize(word)}.join(" ")
+  end
+
+end
