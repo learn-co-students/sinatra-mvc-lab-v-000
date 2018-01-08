677ca4bc3e7266ce07b6b5176d1634f2d75fa1fb
Joshua Needham <joshuabneedham@gmail.com>
2018-01-08 15:05:37 -0700

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,13 @@
 class PigLatinizer
-
+  # Defines a vowel constant array
   VOWELS = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
+  # Splits a sentence into individual words and runs each word through the piglatinize(word) method
   def to_pig_latin(sentence)
     sentence.split.collect { |word| piglatinize(word) }.join(" ")
   end
 
+  # Converts individual words into pig latin including those passed in via the to_pig_latin(sentence) method
   def piglatinize(word)
     return "#{word}way" if VOWELS.include?(word[0])
     word = word.split("")
