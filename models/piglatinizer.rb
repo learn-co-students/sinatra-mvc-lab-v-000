e8626981d5463d63760937bed4c31abdc6a4b0a3
github username <github email address>
2018-01-10 17:31:58 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -54,34 +54,3 @@ class PigLatinizer
         pl_ary.join(' ') 
     end
 end
-
-
-
-# class PigLatinizer
-
-#     def piglatinize(word)
-  
-#       non_pig_latin_words = ["I", "me", "to", "too", "a", "an", "in", "and", "on"]
-#       vowels = ["a", "e", "i", "o", "u"]
-  
-#       if non_pig_latin_words.include?(word)
-#         word
-#       elsif vowels.include? word[0]
-#         word << "ay"
-#       else
-#         consonants = ""
-#         while !vowels.include?(word[0])
-#           consonants << word[0]
-#           word = word.split("")[1..-1].join
-#         end
-#         word + consonants + 'ay'
-#       end
-#     end
-  
-  
-#     def to_pig_latin(string)
-#       string.split.collect{|word| piglatinize(word)}.join(" ")
-#     end
-  
-#   end
-  
