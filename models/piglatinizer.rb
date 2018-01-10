36898752944015d29aea001390ef109bba898329
github username <github email address>
2018-01-10 12:55:53 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,38 +10,44 @@ class PigLatinizer
 
     def piglatinize(words) 
         # binding.pry
-       if words.downcase.chr == "a" || words.downcase.chr == "e" || words.downcase.chr == "i" || words.downcase.chr == "o" || words.downcase.chr == "u"
-            words + "way"      
-        elsif words.match(/^[^aAeEiIoOuU]{3}/)
-            last_char_3 = words.downcase.split(/^.../).slice(1) 
-            first_char_3 = words.downcase.split("")[0..2].join
-            plized = last_char_3 + first_char_3 + "ay"
-            plized
-        elsif words.match(/^[^aeiou]{2}/)
-            last_char_2 = words.downcase.split(/^../).slice(1) 
-            first_char_2 = words.downcase.split("")[0..1].join
-            plized = last_char_2 + first_char_2 + "ay"
-            plized
-        elsif words.match(/^[^aeiou]{1}/)
-            last_char = words.downcase.split(/^./).slice(1) 
-            first_char = words.downcase[0]
-            plized = last_char + first_char + "ay"
-            plized
-
-       
-            # binding.pry
-            
-        # elsif !words.downcase.chr == "a" || !words.downcase.chr == "e" || !words.downcase.chr == "i" || !words.downcase.chr == "o" || !words.downcase.chr == "u" && !words[1] == "a" || !words[1] == "e" || !words[1] == "i" || !words[1] == "o" || !words[1] == "u" 
-        #     last_chars = words.downcase.split(/^./).slice(1) 
-        #     first_char = words.downcase.split(//)[0..1].join("")
-        #     plized = last_chars + first_char + "ay"
-        #     plized
-        # else
-        #     last_chars = words.downcase.split(/^./).slice(1) 
-        #     first_char = words.downcase.split(//)[0]
-        #     plized = last_chars + first_char + "ay"
-        #     plized
+        if words.include?("")
+            words.split("").each do |word| 
+            if word.downcase.chr == "a" || word.downcase.chr == "e" || word.downcase.chr == "i" || word.downcase.chr == "o" || word.downcase.chr == "u"
+                    word + "way"      
+            elsif word.match(/^[^aAeEiIoOuU]{3}/)
+                    last_char_3 = word.downcase.split(/^.../).slice(1) 
+                    first_char_3 = word.downcase.split("")[0..2].join
+                    plized = last_char_3 + first_char_3 + "ay"
+                    plized
+            elsif word.match(/^[^aeiou]{2}/)
+                    last_char_2 = word.downcase.split(/^../).slice(1) 
+                    first_char_2 = word.downcase.split("")[0..1].join
+                    plized = last_char_2 + first_char_2 + "ay"
+                    plized
+            elsif word.match(/^[^aeiou]{1}/)
+                    last_char = word.downcase.split(/^./).slice(1) 
+                    first_char = word.downcase[0]
+                    plized = last_char + first_char + "ay"
+                    plized
+            end
+        else
+            if words.downcase.chr == "a" || words.downcase.chr == "e" || words.downcase.chr == "i" || words.downcase.chr == "o" || words.downcase.chr == "u"
+                words + "way"      
+            elsif words.match(/^[^aAeEiIoOuU]{3}/)
+                last_char_3 = words.downcase.split(/^.../).slice(1) 
+                first_char_3 = words.downcase.split("")[0..2].join
+                plized = last_char_3 + first_char_3 + "ay"
+                plized
+            elsif words.match(/^[^aeiou]{2}/)
+                last_char_2 = words.downcase.split(/^../).slice(1) 
+                first_char_2 = words.downcase.split("")[0..1].join
+                plized = last_char_2 + first_char_2 + "ay"
+                plized
+            elsif words.match(/^[^aeiou]{1}/)
+                last_char = words.downcase.split(/^./).slice(1) 
+                first_char = words.downcase[0]
+                plized = last_char + first_char + "ay"
+                plized
         end
     end
-
 end
