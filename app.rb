cad340f908f2d3909ff253db63a44b1070267423
Richard Chang <rchang9999@yahoo.com>
2018-01-13 03:22:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pl = PigLatinize.new
+    Latinparams[:user_phrase]
+
+  end
+
 end
