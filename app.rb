130f961072dac6a313d327efb0aeb8d6cd4ca15c
rh24 <ms.rebeccahong@gmail.com>
2018-02-06 16:34:31 -0500

    delete require_relative in app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require_relative 'config/environment'
-  require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
 
@@ -9,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     @piglatinized = PigLatinizer.new.to_pig_latin(params[:user_phrase])
-    
+
     erb :results
   end
 end
