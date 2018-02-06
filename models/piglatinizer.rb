0cb31f3fabd616c22abdf4f5dd58791308366365
Sally Zasloff <szasloff@gmail.com>
2018-02-06 15:27:34 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,34 +1,28 @@
 require 'pry'
 class PigLatinizer
 
-
   def piglatinize(word)
-    #words that begin with a consonant
-    if consonant?(word[0])
-      # word = word.slice(1..-1)
-      # word1 = word1 + word[0] + "ay"
 
+    if consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0) + word.slice(1) + word.slice(2) + "ay"
+
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0) + word.slice(1) + "ay"
+
+    elsif consonant?(word[0])
       word = word.slice(1..-1) + word.slice(0) + "ay"
 
-      #words that begin with 2 consanants
-
-    # elsif consonant?(word[0]) && consonant?([1])
-    #   word1 = word.slice(2..-1)
-    #   word1 = word1 + word[0] + word[1] + "ay"
-
-    #words that begin with a vowel(maybe start with vowel and work down to consonants)
-    else
-      word = word + "way"
-    end
+      else
+        word = word + "way"
+      end
   end
 
   def to_pig_latin(sentence)
     sentence.split.collect{|word|piglatinize(word)}.join(" ")
   end
 
-
-  def consonant?(character)
-    !character.match(/[aAeEiIoOuU]/)
+  def consonant?(letter)
+    !letter.match(/[aAeEiIoOuU]/)
   end
 
 
