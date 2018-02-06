d0dac94e492f36295e06efd47a84fdb393afb18a
Sally Zasloff <szasloff@gmail.com>
2018-02-06 02:00:15 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,15 +5,16 @@ class PigLatinizer
   def piglatinize(word)
     #words that begin with a consonant
     if consonant?(word[0])
-      word1 = word.slice(1..-1)
-      word1 = word1 + word[0] + "ay"
-      binding.pry
+      # word = word.slice(1..-1)
+      # word1 = word1 + word[0] + "ay"
+
+      word = word.slice(1..-1) + word.slice(0) + "ay"
 
       #words that begin with 2 consanants
 
-    elsif consonant?(word[0]) && consonant?([1])
-      word1 = word.slice(2..-1)
-      word1 = word1 + word[0] + word[1] + "ay"
+    # elsif consonant?(word[0]) && consonant?([1])
+    #   word1 = word.slice(2..-1)
+    #   word1 = word1 + word[0] + word[1] + "ay"
 
     #words that begin with a vowel(maybe start with vowel and work down to consonants)
     else
