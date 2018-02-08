26a6278ea21402fbc3ccb784d26f4d84c9c3106b
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-02-08 17:57:04 -0500

    ALL TESTS PASSING

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@ require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
     get '/' do
-        user_phrase = params[:user_phrase]
-        @pig_latinized = PigLatinizer.new(user_phrase)
-
         erb :user_input
+    end
+
+    post '/piglatinize' do
+        @user_phrase = params[:user_phrase]
+        @pig_latinizer = PigLatinizer.new
+
+        erb :pig_latinized
     end
 end
