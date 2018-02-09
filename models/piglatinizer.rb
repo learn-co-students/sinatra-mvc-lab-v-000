980240740a02a1f15f24916b03479e585c260565
Dani Schuhman <dani8439@aol.com>
2018-02-09 01:10:16 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class PigLatinizer
+  # Create a constant array of vowels
+  VOWELS = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+
+  # need to split the sentence
+  def to_pig_latin(phrase)
+    # splits the sentence, then collect -- iterate over each word - piglatinize each word - then rejoin the collected words back into a sentence
+    phrase.split.collect{|word| piglatinize(word)}.join(" ")
+  end
+
+  def piglatinize(word)
+    return word << "way" if VOWELS.include?(word[0])
+    word = word.split("")
+
+    until VOWELS.include?(word[0])
+      letter = word.shift
+      word = word << letter
+    end
+
+    word.join + "ay"
+  end
+
+end
+
+
+# def piglatinize(word)
+#   if VOWELS.include?(word[0]) == true
+#     word << "way"
+#   else
+#     last = word.slice(1..-1)
+#     first = word.slice(0)
+#     last + first + "ay"
+#   end
