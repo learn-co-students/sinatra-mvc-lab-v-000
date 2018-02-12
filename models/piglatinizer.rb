9d30781f56962fd633eaf4f9afb1491509316da8
AutumnJ <33694669+AutumnJ@users.noreply.github.com>
2018-02-12 00:59:04 -0600

    Update piglatinizer.rb

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,20 +6,20 @@ class PigLatinizer
 		letter_array = word.split("")
 		counter = 0
 		if vowel_index == 0
-			return word + "way"
+		    return word + "way"
 		else
-			while counter < vowel_index
-				move_letter = letter_array.shift
-				letter_array.push(move_letter)
-				counter += 1
-			end
+		    while counter < vowel_index
+		        move_letter = letter_array.shift
+			letter_array.push(move_letter)
+			counter += 1
+		    end
 		end
-			letter_array << "ay"
-			letter_array.join("")
+		letter_array << "ay"
+		letter_array.join("")
 	end
 
 	def to_pig_latin(sentence)
-		sentence.split.collect { |word| piglatinize(word) }.join(" ")
+	    sentence.split.collect { |word| piglatinize(word) }.join(" ")
 	end
 
 end
