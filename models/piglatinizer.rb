6cb9d5a6183be8ddf23b17e22a36693ef1d193a6
github username <github email address>
2018-02-13 10:22:56 -0700

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+class PigLatinizer
+
+
+  def self.piglatinize(word)
+    if word =~ (/\A[aeiou]/i)
+      word = word + 'way'
+    elsif word =~ (/\A[^aeiou]/i)
+      match = /\A[^aeiou]/i.match(word)
+      word = match.post_match + match.to_s + 'ay'
+      end
+    word
+  end
+
+  def self.to_pig_latin(text)
+    words = text.split(" ")
+    sentence = []
+    words.each do |word|
+      sentence << word.piglatinize
+    end
+    sentence.join(" ")
+  end
+
+end
