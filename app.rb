58a4be0c244b41c02491b9d30b9b3b82ec91e95a
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-14 05:51:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @pig_latin = PigLatinizer.new
+    erb :results
+  end
 end
