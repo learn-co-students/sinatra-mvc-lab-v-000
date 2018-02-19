279f6a25c28a8e84a7255510faa45778f9405fbe
Blakeley Bond <burrick2003@yahoo.com>
2018-02-19 23:54:42 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,12 +3,23 @@ class PigLatinizer
   def initialize(input)
     @input = input.downcase
   end
-
-  def piglatinize
-
+  def piglatinize(word)
+      vowels = %w{a e i o u}
+      word.each_char do |chr|
+      index = word.index(chr)
+      if index != 0 && vowels.include?(chr.downcase)
+        consonants = word.slice!(0..index-1)
+        return word + consonants + "ay"
+      elsif index == 0 && vowels.include?(chr.downcase)
+        return word + "ay"
+      end
+    end
   end
 
-
+  def to_pig_latin
+    sentence=@input
+    sentence.split(" ").collect { |word| piglatinize(word) }.join(" ")
+  end
 
 
 
