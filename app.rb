e9ecf8f13de200f323ce3edc169ffd44b520739a
Miwa <paintfx@gmail.com>
2018-02-24 17:18:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @output = PigLatinize.new.piglatinizer(params[:user_input])
+    @output = PigLatinizer.new.to_pig_latin(params[:user_phrase])
     erb :output
   end
 end
