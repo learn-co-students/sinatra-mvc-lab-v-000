dbfe1e2b6e50cabb720efcfefde4a84861f4a2c8
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-27 15:34:03 -0500

    Create pig latin web app

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require_relative "models/piglatinizer.rb"
 
 class App < Sinatra::Base
+
+  get "/" do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    # puts text_from_user = params[:user_phrase]
+    @pig_latin = PigLatinizer.new.to_pig_latin(params[:user_phrase])
+
+    erb :results
+  end
+
 end
