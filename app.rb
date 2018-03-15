9e6ba2a64ebaedbea8ea5a59f48dce6b1470f1a7
johnwojo <jwojo13@gmail.com>
2018-03-15 11:01:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,11 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
     get '/' do
-    erb :user_input
+      erb :user_input
     end
 
     post '/piglatinize' do
-      @pig_latin = PigLatinizer.new.translate(params[:user_phrase])
+      pig = PigLatinizer.new
+      @pig_latin = pig.to_pig_latin(params[:user_phrase])
       erb :results
     end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,30 +1,22 @@
 class PigLatinizer
-
-
-  def begins_with_consonant?(text)
-    vowels = ["A","a","e","E","i","I","o","O","u","U"]
-    !vowels.include?(text)
-
-  end
-
   def piglatinize(word)
-    if !begins_with_consonant?(word)[0]
-      word = word + "way"
-
-
-    elsif begins_with_consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2) + "ay"
-
-
-    elsif begins_with_consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3) + "ay"
-
-    else word = word.slice(1..-1) + word.slice(0,2) + "ay"
+    if !consonant?(word[0])
+      word = word + "w"
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    else
+      word = word.slice(1..-1) + word.slice(0)
     end
-    word
+    word << "ay"
   end
 
-  def translate(sentence)
-    sentence.split.collect { |word| piglatinize(word).join(" ")}
+  def consonant?(text)
+    !text.match(/[aAeEiIoOuU]/)
+  end
+
+  def to_pig_latin(sentence)
+    sentence.split.collect {|word| piglatinize(word)}.join(" ")
   end
 end
