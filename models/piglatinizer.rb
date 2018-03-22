af4baa44aabab75d9765d8f3f4575e71b144da77
Eden Mesfun <emesfun@gmail.com>
2018-03-22 16:15:59 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,54 +3,52 @@ class PigLatinizer
 
   attr_accessor :word
 
-  @@vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
 
 
-  def initialize
+  def initialize(word)
     @word = word
   end
 
   def piglatinize(word)
 
+    consonants = ['B','b','C','c','D','d','F','f', 'G','g', 'H','h','J','j','K','k','L','l','M','m', 'N','n','P','p','Q','q','R','r','S','s','T','t','V','v','X','x','Z','z']
+
+    vowels = ['a','A','e','E','i','I','o','O','u','U']
+
     array = word.split("")
+    first_letter = array[0]
+    second_letter = array[1]
+    third_letter = array[2]
 
-      if array[0] == "a" || array[0] == "A" || array[0] == "e" || array[0] == "E"
-        first_letter = array[0]
+    if first_letter.start_with?(vowels.to_s) && array.length == 1
         array.shift
         new_array = array.push(first_letter)
         new_word = new_array.push("w", "a", "y").join("")
         return new_word
-      elsif array[0] == "i" || array[0] == "I" || array[0] == "o" || array[0] == "O" || array[0] == "u" || array[0] == "U"
-        first_letter = array[0]
+    elsif first_letter.start_with?(consonants.to_s) && second_letter.start_with?(vowels.to_s)
         array.shift
         new_array = array.push(first_letter)
-        new_word = new_array.push("w", "a", "y").join("")
+        new_word = new_array.push("a", "y").join("")
         return new_word
-      end
-
-      @@vowels.each do |letter|
-          if array[0] != letter && array[1] != letter
-          first_letter = array[0]
-          second_letter = array[1]
-          array.shift
-          array.shift
-          new_array = array.push(first_letter)
-          new_array = array.push(second_letter)
-          new_word = new_array.push("a", "y").join("")
+    elsif first_letter.start_with?('s','S') && second_letter.start_with?('p','P') && third_letter.start_with?('r','R')
+        array.shift
+        array.shift
+        array.shift
+        new_word = array.push("a", "y").join("")
+        return new_word
+    elsif first_letter.start_with?(vowels.to_s) && second_letter.start_with?(consonants.to_s)
+          new_word = array.push("w", "a", "y").join("")
           return new_word
-          end
-      end
-
-        @@vowels.each do |letter|
-          if array[0] != letter && array[1] == letter
-          first_letter = array[0]
-          array.shift
-          new_array = array.push(first_letter)
-          new_word = new_array.push("a", "y").join("")
-          return new_word
-          end #if iteration
-        end #each iteration
-
+    else first_letter.start_with?(consonants.to_s) && second_letter.start_with?(consonants.to_s)
+        first_letter = array[0]
+        second_letter = array[1]
+        array.shift
+        array.shift
+        new_array = array.push(first_letter)
+        new_array = array.push(second_letter)
+        new_word = new_array.push("a", "y").join("")
+        return new_word
+      end #if statement
   end #method
 
 end
