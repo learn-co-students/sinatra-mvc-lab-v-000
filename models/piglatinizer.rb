67041c5f85cd73b74a3a005781e18833ae0a25b6
Eden Mesfun <emesfun@gmail.com>
2018-03-22 19:26:50 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -19,7 +19,9 @@ class PigLatinizer
     first_letter = split_word[0]
     second_letter = split_word[1]
     third_letter = split_word[2]
-      if @vowels.include?(first_letter) && split_word.length == 1
+      if split_word.include?(" ")
+         piglatinize_sentence(word)
+      elsif @vowels.include?(first_letter) && split_word.length == 1
         new_word = first_letter.capitalize + "way"
         return new_word
       elsif @consonants.include?(first_letter) &&
@@ -56,4 +58,29 @@ class PigLatinizer
         return new_word
       end #if statement
   end #method
+
+  def piglatinize_sentence(sentence)
+    @consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','x','z','w','y']
+    @consonants_caps = ['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','X','Z','W','Y']
+    @vowels = ['a', 'e', 'i', 'o', 'u']
+    @vowels_caps = ['A', 'E', 'I', 'O', 'U']
+
+    array = sentence.split(" ")
+
+    pig_latin = []
+
+    array.each_with_index do |word, index|
+        if @vowels.include?(word[0]) && word.length == 1
+        new_word = word[0] + "way"
+        pig_latin << new_word
+        elsif @consonants.include?(word[0]) && @vowels.include?(word[1])
+        split_word = word.split("")
+        split_word.shift
+        new_word = split_word.push(word[0]).join("") + "ay"
+        pig_latin << new_word
+        else @consonants.include?(word[0]) && @consonants.include?(word[1]) && @vowels.include?(word[2])
+        end #if statement
+    end #each iterator
+  end #method
+
   end
