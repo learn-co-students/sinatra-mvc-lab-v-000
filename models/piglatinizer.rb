0cff0ec58c94faa9aad8cd10dc2b458df79ec9ad
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-29 07:03:52 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,30 @@
+class PigLatinizer
+
+  def initialize
+  end
+
+  def piglatinize(phrase)
+    words = phrase.split
+    piglatinized_phrase = ""
+    #binding.pry
+    words.each do |word|
+      piglatinized_phrase << word_to_piglatinize(word) + " "
+      #binding.pry
+    end
+    piglatinized_phrase.strip
+  end
+
+   def word_to_piglatinize(word)
+     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+     if vowels.include? word[0]
+       word + "way"
+     elsif !vowels.include?(word[0]) && !vowels.include?(word[1]) && !vowels.include?(word[2])
+       word[3..-1] + word[0..2] + "ay"
+     elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
+       word[2..-1] + word[0..1] + "ay"
+     else !vowels.include?(word[0])
+       word[1..-1] + word[0] + "ay"
+    end
+  end
+
+end
