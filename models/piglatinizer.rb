cfe4dba33549b4687a8376e4549f1228907d2d50
Carol Greene <carolgreene3@gmail.com>
2018-04-07 17:15:40 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,32 +3,22 @@ require 'pry'
 class  PigLatinizer
 
 
-  #attr_reader :words
-
-  #def split_phrase(phrase)
-    #new_phrase = phrase.split(" ")
-  #end
-
   def to_pig_latin(phrase)
     words = phrase.split(" ")
-    words.map! {|word| piglatinize(word)}
-    words.join(" ")
+    words.map! {|word| piglatinize(word)}.join(" ")
   end
 
   def piglatinize(word)
     if word.include?(" ")
       to_pig_latin(word)
     else
-      vowels = %w{a e i o u}
-      word.each_char do |chr|
-        index = word.index(chr)
-        if index != 0 && vowels.include?(chr.downcase)
-          consonants = word.slice!(0..index-1)
-          return word + consonants + "ay"
-        elsif index == 0 && vowels.include?(chr.downcase)
-          return word + "way"
-        end
-        end
+      first_split = word.split(/([aeiouAEIOU].*)/)[0]
+      second_split = word.split(/([aeiouAEIOU].*)/)[1]
+      if first_split == ""
+        second_split + "way"
+      else
+        second_split + first_split + "ay"
       end
     end
   end
+end
