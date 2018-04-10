de18347f6b58ffda19f0136c144cc331b490f0d7
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-10 21:01:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @user_phrase = params
-    erb :piglatinize
+    @piglatized_text = params["user_phrase"]
+    erb :results
   end
 end
