523dc51a31076600ca2ecf25eb6de2652fbb4b70
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-12 19:03:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    @string = (params[:user_phrase])
+    @piglatin = PigLatinizer.new
+    erb :pig_latin
+  end
 end
