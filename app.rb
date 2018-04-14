1b7b740aaac67ce4cbd9f26b58a792bb4cddeed3
Ariana Berger <ariana.berger@gmail.com>
2018-04-14 02:11:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @text_to_convert = PigLatinizer.new(params[:user_text])
+    erb :piglatinize
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+class PigLatinizer
+
+  # attr_accessor :text
+
+  def initialize
+    # @text = text.downcase
+  end
+
+  def piglatinize(word)
+    # text.split > convert to pig latin
+    word.upcase
+  end
+
+
+end
