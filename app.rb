d9ed8b08deaf14c454cf6cae840935945489f83a
Raiet Intraub <raietsaeed@gmail.com>
2018-05-10 02:15:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    # @converted_text = PigLatinizer.new(params[:user_phrase])
+    # erb :piglatinize
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+class PigLatinizer
+
+  attr_reader :text
+
+  def show_text
+    @text
+  end
+
+  def piglatinize(words)
+    @text = words
+    # Checks number of words in phrase (if more than one)
+    # Converts each or single word into Pig Latin
+    # Returns converted phrase
+  end
+
+  def check_phrase(phrase)
+    # Checks number of words in phrase (if more than one)
+  end
+
+  def convert_word(word)
+    # Converts each or single word into Pig Latin
+  end
+
+end
