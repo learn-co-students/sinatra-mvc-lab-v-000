fb3843ce76b2e2ab7f62f24809f04ba07f4a219d
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-14 19:27:04 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,15 @@ class PigLatinizer
 
   def piglatinize(string)
     if string[0].scan(/[aeiou]/) == [] #starts with vowel?
+      if string[1].scan(/[aeiou]/) == [] #starts with constenant cluters?
+        c = string[0..1]
+        string.slice!(c)
+        string = string + c + "ay"
+      else
+        c = string[0]
+        string.slice!(c)
+        string = string + c + "ay"
+      end
     end
   end
 end
