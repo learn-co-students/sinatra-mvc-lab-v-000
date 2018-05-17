e3efd4b912e032d2cacf616b27001492953efd3a
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-17 20:03:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
-    @translated_text = PigLatinizer.new(params[:user_text])
+  post '/piglatinize' do
+    @text = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :results
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,24 @@
 class PigLatinizer
-  attr_reader :text
 
-  def initialize(text)
-    @text = text
+  def initialize()
   end
 
-  def translate
-    text.upcase
+  def piglatinize (text)
+    words = text.split(' ')
+    words = words.map! do | word |
+      translate(word)
+    end
+    words.join(' ')
+  end
+
+  def translate (word)
+    start_consonants = word.downcase.split(/([aeiou].*)/).first.length
+    if start_consonants == 0
+      new_word = word.concat("way")
+    else
+      new_word = word[start_consonants..word.length-1] + word[0,start_consonants] + "ay"
+    end
+    new_word
   end
 
 end
