d874df21a48f041f9572f924065e9f7007aa734b
Annie Bourke <aabourke@gmail.com>
2018-05-18 18:05:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/user_input' do
-    erb :user_input
+  get '/' do
+    erb :user_index
   end
 
-  post '/user_input' do
-    @translated_text = PigLatinizer.new(params[:user_text])
+  post '/piglatinize' do
+    @translated_text = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :results
   end
 
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,18 @@
 class PigLatinizer
-  attr_reader :text
 
-  def initialize(text)
-    @text = text.downcase
-  end
-
-  def translate
+  def piglatinize(text)
     text_array = text.split(" ").to_a
     translated_array = []
 
     text_array.each do |word|
-      alphabet = ('a'..'z').to_a
-      vowels = ["a", "e", "i", "o", "u"]
+      alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
+      vowels = ["a", "e", "i", "o", "u", "A", "I", "E", "O", "U"]
       consonants = alphabet - vowels
 
       if vowels.include?(word[0])
-        translated_array << (word + 'ay')
+        translated_array << (word + 'way')
+      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        translated_array << (word[3..-1] + word[0..2] + 'ay')
       elsif consonants.include?(word[0]) && consonants.include?(word[1])
         translated_array << (word[2..-1] + word[0..1] + 'ay')
       elsif consonants.include?(word[0])
@@ -26,5 +23,4 @@ class PigLatinizer
     end
       translated_array.join(" ")
   end
-
 end
