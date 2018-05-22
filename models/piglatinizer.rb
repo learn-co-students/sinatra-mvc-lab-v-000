77614b79cbde73dedad3bc6d8bc258913aef77ac
JuanGongora <gongora.animations@gmail.com>
2018-05-22 15:58:51 -0400

    Second try to see why rpsec ignores approval

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,28 @@ class PigLatinizer
 
   def convert
     current = @phrase.split(" ")
-    current.map {|word| word << "way"}
+
+    current.map do |word|
+
+      # If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
+      if word =~ /\b[^aeiouAEIOU][aeiou]/
+        consonant = word[0]
+        stripped_word = word[1..-1].strip
+        word.clear
+        word << stripped_word << consonant << "ay"
+
+      # If a word starts with two consonants move the two consonants to the end of the word and add "ay."
+      elsif word =~ /\b[^aeiouAEIOU][^aeiou]/
+        consonants = word[0..1]
+        stripped_word = word[2..-1].strip
+        word.clear
+        word << stripped_word << consonants << "ay"
+
+      # If a word starts with a vowel add the word "way" at the end of the word.
+      elsif word =~ /\b[aeiouAEIOU]/
+        word << "way"
+      end
+    end
     @phrase = current.join(" ")
   end
 
