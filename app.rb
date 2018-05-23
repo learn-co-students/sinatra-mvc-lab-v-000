0a1a1eb5d20278343978af1da7cf0219224a2e1c
Veronica Kim <vero.a.kim@gmail.com>
2018-05-23 02:41:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @phrase = params[:user_phrase]
+    @pigger = PigLatinizer.new
+
+    erb :return
+  end
+
 end
