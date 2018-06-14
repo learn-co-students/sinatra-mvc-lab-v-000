45fd4c7618b4de295f48f652339df1aa1689efad
Alex Wilson <fonzmp@gmail.com>
2018-06-14 18:42:46 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,26 +2,37 @@ require 'pry'
 
 class PigLatinizer
 
-  def piglatinize(word)
-    vowels = ["a", "e", "i", "o", "u"]
-    word_split = word.split('')
-    first = []
-    word_split.each do |letter|
-      if vowels.include?(letter)
-        first << letter
-      else
-        exit
+  def piglatinize(string)
+    string_split = string.split(" ")
+    holder = []
+    if string_split.size > 1
+      string_split.each do |word|
+        new_word = piglatinize_word("#{word}")
+        holder << new_word
       end
+      holder.join(' ')
+    else
+      piglatinize_word(string)
     end
+  end
 
-    # if vowels.include? word_split[0].downcase
-    #   @piggy = word + "way"
-    # else
-    #   first = word_split.shift
-    #   start_of_word = word_split.join("")
-    #   @piggy = start_of_word + first + "ay"
-    # end
-    # @piggy
+  def piglatinize_word(word)
+    word_split = word.split('')
+    first_vowel = word_split.find_index {|x| vowel?(x)}
+    if first_vowel.to_i > 0
+      leading_letters = word_split[0..first_vowel-1]
+      remainder_of_word = word_split[first_vowel..-1]
+      @pig = remainder_of_word.join('') + leading_letters.join("") + "ay"
+    else
+      @pig = word_split.join("") + "way"
+    end
+    @pig
+  end
+
+  def vowel?(letter)
+    vowels = ["a", "e", "i", "o", "u"]
+    downcased = letter.downcase
+    vowels.include?(downcased)
   end
 
 end
