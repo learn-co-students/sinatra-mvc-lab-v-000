2860c835b3d70567b4838bc2fd9097dd6ae351a2
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-11 21:44:41 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,5 @@
+# https://en.wikipedia.org/wiki/Pig_Latin#Rules
+# https://github.com/BrennanFulmer/sinatra-integrating-models-walkthrough-v-000
 
 class PigLatinizer
   attr_reader :text
@@ -9,9 +11,11 @@ class PigLatinizer
   def pig_latinize
     vowels = ['a', 'e', 'i', 'o', 'u']
     
-    # https://en.wikipedia.org/wiki/Pig_Latin#Rules
+    # grab consonants
     
-    # https://github.com/BrennanFulmer/sinatra-integrating-models-walkthrough-v-000
+    # starts with vowel
+    
+
   end
   
 end
