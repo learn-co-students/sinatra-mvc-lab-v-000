10328ec48da580244ec8802e154fb4ef1516c32d
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-11 18:16:16 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+
+class PigLatinizer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text
+  end
+  
+  def pig_latinize
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    # https://en.wikipedia.org/wiki/Pig_Latin#Rules
+  end
+  
+end
