403c87addd706dbb38c5a7a7105482c7f4739022
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-12 01:53:07 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,28 +1,41 @@
 
 class PigLatinizer
-  attr_reader :text
 
-  def piglatinize(phrase)
+  def piglatinize(text)
     vowels = ['a', 'e', 'i', 'o', 'u']
     
-    
-    seperated = phrase.split(' ')
-    
-    formatted = seperated.collect do |word| 
-      if vowels.include?(phrase[0])  
-        matched = phrase.match(/^[aeiou]+/)
+    if text.include?(' ')
+      seperated = text.split(' ')
+      
+      formatted = seperated.collect do |word| 
+        if vowels.include?(text[0])  
+          matched = text.match(/^[aeiou]+/i)
+          back = matched.post_match
+          front = matched[0]
+          "#{back}#{front}way"
+        else
+          matched = text.match(/^[^aeiou]+/i)
+          back = matched.post_match
+          front = matched[0]
+          "#{back}#{front}ay"
+        end
+      end
+      
+      formatted.join(' ')
+    else
+      if vowels.include?(text[0])  
+        matched = text.match(/^[aeiou]+/i)
         back = matched.post_match
         front = matched[0]
         "#{back}#{front}way"
       else
-        matched = phrase.match(/^[^aeiou]+/)
+        matched = text.match(/^[^aeiou]+/i)
         back = matched.post_match
         front = matched[0]
         "#{back}#{front}ay"
       end
     end
-    
-    formatted.join(' ')
+
   end
   
 end
