0e8cd38668326fd89b55aa1a0b33af0160418cf9
Carrie Savinell <carrielane89@gmail.com>
2018-07-14 14:16:43 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,30 +2,19 @@ class PigLatinizer
 
   attr_accessor :text
 
-
   def piglatinize(text)
-    @text = text
-    #split words in an array
-    text_array = @text.split(" ")
-
-    #iterate over words and add
-    new_array = text_array.collect do |word|
-      if word.slice(0,1) =~ (/[AEIOUaeiou]/)
-        word = word + "way"
-      else
-        word = word.slice(1,word.size) + word.slice(0,1)
-        if  word.slice(0,1) =~ (/[aeiou]/)
-          word = word + "ay"
+    @text = text.downcase
+    vowel ='aeiou'
+    text = text.split()
+      text.map do |word|
+        if word.length == 1
+            "#{word}way"
+        elsif vowel.include? word[0].downcase
+            "#{word}way"
         else
-          word = word.slice(1,word.size) + word.slice(0,1)
-          if word.slice(0,1) =~ (/[aeiou]/)
-            word = word + "ay"
-          else
-            word = word.slice(1, word.size) + word.size(0,1) + "ay"
-          end
+            split = word.split(/([aeiou])/,2)
+            "#{split[1]}#{split[2]}#{split[0]}ay"
         end
-      end
-    end
-  new_phrase = word.join(" ")
- end
+    end.join(' ')
+   end
 end
