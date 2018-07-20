21a161da8c11b614325425ebb9950bbaa778be23
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-20 04:58:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require_relative 'config/environment'
-require_relative 'models/piglatinizer'
-class App < Sinatra::Base
 
-  get '/' do
+class App < Sinatra::Base
+  get "/" do
     erb :user_input
   end
 
-  post '/piglatinize' do
-    @phrase = PigLatinizer.new.to_pig_latin(params[:user_phrase])
-    erb :converted_input
+  post "/piglatinize" do
+    user_input = PigLatinizer.new
+    @piglatin = user_input.piglatinize(params[:user_phrase])
+    erb :results
   end
+  
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,29 +1,21 @@
 class PigLatinizer
 
-  def piglatinize(string)
-    string = string.split('')
-    if string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[1].downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[2].downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-      first = string.shift
-      second = string.shift
-      third = string.shift
-      "#{string.join}#{first}#{second}#{third}ay"
-    elsif string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[1].downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-      first = string.shift
-      second = string.shift
-      "#{string.join}#{first}#{second}ay"
-    elsif string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-      first = string.shift
-      "#{string.join}#{first}ay"
-    elsif string.first.downcase.match(/[aeoui]/)
-      "#{string.join}way"
-    else
-      string.join
-    end
-  end
+   def piglatinize(sentence)
+     if sentence.include?(" ")
+       split_sentence(sentence).join(" ")
+     elsif ["a","e","i","o","u"].include?(sentence[0].downcase)
+       sentence + "way"
+     else
+        parts = sentence.split(/([aeiou].*)/)
+        parts[1]+parts[0]+"ay"
+     end
+   end
 
-  def to_pig_latin(sentence)
-    sentence.split(' ').map do |value|
-      self.piglatinize(value)
-    end.join(' ')
-  end
+   def split_sentence(sentence)
+     array = sentence.split(" ")
+     array.map do |word|
+       piglatinize(word)
+     end
+   end
+
 end
