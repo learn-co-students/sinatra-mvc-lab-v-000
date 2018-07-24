aceb59a6d69093fb6a721790a80bf0d8088b3671
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-24 05:07:19 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,33 @@
-binding.pry
 class PigLatinizer
-
-  def piglatinize(word)
-    binding.pry
-    if word.include?("/\A+[bcdfghjklmnpqrstvwxyz]/")
-      arr = word.drop(1)
-      arr << word[0]
-      "#{arr}ay"
-      binding.pry
+  
+  def piglatinize(string)
+    if string.match(/\s/)
+      words = string.split(" ")
+      piglatin_array = words.collect do |word|
+        piglatinize_string(word)
+      end
+      piglatin_array.join(" ")
+    else
+      piglatinize_string(string)
     end
   end
+  
+  def piglatinize_string(string)
+    if string.match(/\A+[bcdfghjklmnpqrstvwxyz]{3}/)
+      new_string = string[3..-1]
+      new_string << string[0..2]
+      "#{new_string}ay"
+    elsif string.match(/\A+[bcdfghjklmnpqrstvwxyz]{2}/)
+      new_string = string[2..-1]
+      new_string << string[0..1]
+      "#{new_string}ay"
+    elsif string.match(/\A+[bcdfghjklmnpqrstvwxyz]/)
+      new_string = string[1..-1]
+      new_string << string[0]
+      "#{new_string}ay"
+    elsif string.downcase.match(/\A+[aeiou]/)
+      "#{string}way"
+    end
+  end
+    
 end
