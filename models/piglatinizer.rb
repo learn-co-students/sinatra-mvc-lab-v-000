b2d62fed553bf7c376c5064cf45396d9daeb5ef0
Nancy Jo <enciie@gmail.com>
2018-08-02 06:21:33 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,11 +9,11 @@ class PigLatinizer
   end
 
   def vowel?(char)
-    char.downcase == "a" || char.downcase == "e" || char.downcase == "i" || char.downcase == "o" || char.downcase == "u"
+    char.match(/[aAeEiIoOuU]/)
   end
 
   def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
+    !vowel?(char)
   end
 
   def piglatinize_word(word)
@@ -23,15 +23,15 @@ class PigLatinizer
     #words that start with 3 consonants
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
-      word << "ay"
+      word + "ay"
     #words that start with 2 consonants
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
-      word << "ay"
+      word + "ay"
     #words that start with 1 consonant
     else
       word = word.slice(1..-1) + word.slice(0)
-      word << "ay"
+      word + "ay"
     end
   end
 
