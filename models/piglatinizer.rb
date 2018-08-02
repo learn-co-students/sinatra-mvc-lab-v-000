f6984ff0629abf7f3d2e2fb868ed8de320a01da7
Nancy Jo <enciie@gmail.com>
2018-08-02 05:53:25 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,41 +2,42 @@ require 'pry'
 class PigLatinizer
 
   def piglatinize(text)
-      pl_phrase = (text.split(" ").length == 1) ? piglatinize_word(text) : piglatinize_sentence(text)
-      puts pl_phrase
-      pl_phrase
-    end
+    @piglatin_words = []
+    words = text.split(" ")
+    words.each {|word| @piglatin_words << piglatinize_word(word)}
+    @piglatin_words.join(" ")
+  end
 
-    def vowel?(char)
-      char.downcase == "a" || char.downcase == "e" || char.downcase == "i" || char.downcase == "o" || char.downcase == "u"
-    end
+  def vowel?(char)
+    char.downcase == "a" || char.downcase == "e" || char.downcase == "i" || char.downcase == "o" || char.downcase == "u"
+  end
 
-    def consonant?(char)
-      !char.match(/[aAeEiIoOuU]/)
-    end
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
 
-    def piglatinize_word(word)
-      #words that start with vowels
-      if vowel?(word[0])
-        word = word + "way"
-      #words that start with 3 consonants
-      elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-        word = word.slice(3..-1) + word.slice(0,3)
-        word << "ay"
-      #words that start with 2 consonants
-      elsif consonant?(word[0]) && consonant?(word[1])
-        word = word.slice(2..-1) + word.slice(0,2)
-        word << "ay"
-      #words that start with 1 consonant
-      else
-        word = word.slice(1..-1) + word.slice(0)
-        word << "ay"
-      end
+  def piglatinize_word(word)
+    #words that start with vowels
+    if vowel?(word[0])
+      word = word + "way"
+    #words that start with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+      word << "ay"
+    #words that start with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+      word << "ay"
+    #words that start with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+      word << "ay"
     end
+  end
 
-    def piglatinize_sentence(sentence)
-      sentence.split.collect {|word| piglatinize_word(word) }.join(" ")
-    end
+  def piglatinize_sentence(sentence)
+    sentence.split.collect {|word| piglatinize_word(word) }.join(" ")
+  end
 
 
 end
