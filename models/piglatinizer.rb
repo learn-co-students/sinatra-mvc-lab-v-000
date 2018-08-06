ec660d9358983b40b87d31b110f11b979bfb28fb
Emily Welsch <emwelsch@gmail.com>
2018-08-06 00:24:47 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,42 @@
 class PigLatinizer
 
-  # def initialize(phrase)
-  #   @phrase = phrase.downcase.piglatinize(phrase)
+  # def piglatinize(phrase)
+  #   arr = phrase.downcase.split(" ")
+  #   arr2 = arr.collect {|word| word.split("").rotate(1).join<<"ay"}
+  #   arr2.join(" ")
   # end
 
-  def piglatinize(phrase)
-    arr = phrase.downcase.split(" ")
-    arr2 = arr.collect {|word| word.split("").rotate(1).join<<"ay"}
-    arr2.join(" ")
-  end
+  def piglatinize(input_str)
+      x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+      puts x
+      x
+    end
+
+    private
+
+    def consonant?(char)
+      !char.match(/[aAeEiIoOuU]/)
+    end
+
+    def piglatinize_word(word)
+      # word starts with vowel
+      if !consonant?(word[0])
+        word = word + "w"
+      # word starts with 3 consonants
+      elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+        word = word.slice(3..-1) + word.slice(0,3)
+      # word starts with 2 consonants
+      elsif consonant?(word[0]) && consonant?(word[1])
+        word = word.slice(2..-1) + word.slice(0,2)
+      # word starts with 1 consonant
+      else
+        word = word.slice(1..-1) + word.slice(0)
+      end
+      word << "ay"
+    end
+
+    def piglatinize_sentence(sentence)
+      sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+    end
+
 end
