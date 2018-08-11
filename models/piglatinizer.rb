d459d248a6fdd40336ab876f1f2483f771e24cf8
ValerieHMoore <periwinkle17@outlook.com>
2018-08-11 16:48:04 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+    attr_reader :words
+   
+    def piglatinize(words)
+        latinized = []
+        words = words.split(" ")
+        words.each do |word|
+            if word =~ (/\A[aeiou]/i)
+                word = word + 'way'
+                latinized << word
+            elsif word =~ (/\A[^aeiou]/i)
+            match = /\A[^aeiou]+/i.match(word)
+            word = match.post_match + match.to_s + 'ay'
+            latinized << word
+            end
+        end
+        latinized.join(" ")
+    end
+
+end
