99290555980d7dd868f699174ee89c1023994af3
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-19 14:02:49 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,13 @@
 class PigLatinizer
 
   def piglatinize(word)
-    if word.include?("ay")
-      word
+    vowels = ["A", "a", "e", "E", "i", "I", "o", "O", "U", "u"]
+    if vowels.include?(word.split("")[0])
+      word + "way"
+    elsif vowels.include?(word.split("")[1])
+      word.split("").rotate.join("") + "ay"
     else
-      word.split("").drop << "ay".join("")
+      word.partition(/[AaEeIiOoUu]/).rotate.join("") + "ay"
     end
   end
-
 end
