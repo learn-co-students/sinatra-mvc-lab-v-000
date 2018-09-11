335c0c48b47c929ed57398c8bd20ce756b419bf4
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-11 12:06:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
-    @piglatinized_text = PigLatinizer.new(params[:user_text])
+  post '/piglatinize' do
+    @piglatinized_text = PigLatinizer.new.piglatinize(params[:user_phrase])
 
     erb :results
   end
