5c5d454f04b1f259795c782345f28ae5dad322ff
Nathan  Visak <nathan.visak@gmail.com>
2018-09-15 18:10:28 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,16 +3,26 @@ class PigLatinizer
   attr_accessor :words
 
 
-  def piglatinize(user_input)
-    user_input = gets.chomp
-    split_words = user_input.split_words
-    #Need to take the first element and make it the last element here
-    split_words.rotate(1)
-    split_words.push("a")
-    split_words.push("y")
-    binding.pry
-
+  def piglatinize(string)
+    word_array = string.split(" ")
+    new_array = word_array.map do |word|
+      new_word = piglatinize_word(word)
+    end
+    new_array.join(" ")
   end
 
+    def piglatinize_word(word)
+      array = word.split("")
+        if array[0] =~ /[aeiouAEIOU]/
+          word + "way"
+        else
+          first_vowel = array.find_index { |e| e.match( /[aeiouAEIOU]/ ) } #returns the index of the first vowel
+          first_chunk = array[0...first_vowel].join("") #This first_chunk will be a consonant cluster
+          last_chunk = word.slice(first_vowel..-1)
+          last_chunk + first_chunk + "a" + "y"
+        end
+    end
 
-end
+
+
+end #ends the class
