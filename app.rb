dd49987aba95e38e48c7486e9417f06ce57e187a
amitnyc83 <amit_nyc@live.com>
2018-09-25 13:30:13 -0400

    Completed test

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+
+
+
+
+post '/piglatinize' do
+ @text = PigLatinizer.new
+ @converted_text = @text.piglatinize(params[:user_phrase])
+
+  erb :piglatinize
+end
+
 end
