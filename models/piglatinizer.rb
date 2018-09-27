02596031f9e87008ef46ce192eea312c54124264
Christia T <christiatidwell@bellsouth.net>
2018-09-27 02:56:38 +0000

    improving special cases of pig latin in model

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,23 @@
 class PigLatinizer
 
-attr_reader :input, :multi_words, :word
+attr_reader :multi_words, :input
 
   def piglatinize(multi_words)
-    word_count = multi_words.split.size
-    if word_count > 1
     multi_words.split.map do |word|
       translate(word)
       end.join(" ")
-    end
   end
 
   def translate(input)
     pig_string = " "
-    if input[0] =~ /[aeiou]/
+    if input[0] =~ /[aeiouAEIOU]/
       return input + 'way'
-    elsif input[0] =~ /[^aeiou]/ && input[1] =~ /[aeiou]/
+    elsif input[0] =~ /[^aeiouAEIOU]/ && input[1] =~ /[aeiouAEIOU]/
       return input[1..-1] + input[0] + 'ay'
-    elsif input[0..1] =~ /[^aeiou]/
+    elsif input[0..1] =~ /[^aeiouAEIOU]/
       return input[2..-1] + input[0..1] + 'ay'
     else
-      return input[1..-1] + input[0] + 'ay'
+      return input[0] + input + 'ay'
     end
   end
 end
