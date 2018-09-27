f4208d18d2b9b0b3467242de33a9ee97f293b26f
Christia T <christiatidwell@bellsouth.net>
2018-09-27 02:40:50 +0000

    passed one more test

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,8 +2,17 @@ class PigLatinizer
 
 attr_reader :input, :multi_words, :word
 
-  def piglatinize(input)
+  def piglatinize(multi_words)
+    word_count = multi_words.split.size
+    if word_count > 1
+    multi_words.split.map do |word|
+      translate(word)
+      end.join(" ")
+    end
+  end
 
+  def translate(input)
+    pig_string = " "
     if input[0] =~ /[aeiou]/
       return input + 'way'
     elsif input[0] =~ /[^aeiou]/ && input[1] =~ /[aeiou]/
@@ -11,14 +20,7 @@ attr_reader :input, :multi_words, :word
     elsif input[0..1] =~ /[^aeiou]/
       return input[2..-1] + input[0..1] + 'ay'
     else
-
+      return input[1..-1] + input[0] + 'ay'
     end
-  end
-
-
-  def piglatinize_words(multi_words)
-    multi_words.split.map! do |word|
-      piglatinize(word)
-    end.join(" ")
   end
 end
