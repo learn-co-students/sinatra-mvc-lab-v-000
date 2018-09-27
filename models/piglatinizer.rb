7e7d3de4641ff11d8da2e99da4c84fd52a974448
Christia T <christiatidwell@bellsouth.net>
2018-09-27 03:01:46 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,10 +14,12 @@ attr_reader :multi_words, :input
       return input + 'way'
     elsif input[0] =~ /[^aeiouAEIOU]/ && input[1] =~ /[aeiouAEIOU]/
       return input[1..-1] + input[0] + 'ay'
-    elsif input[0..1] =~ /[^aeiouAEIOU]/
+    elsif input[0..1] =~ /[^aeiouAEIOU]/ && input[2] =~ /[aeiouAEIOU]/
       return input[2..-1] + input[0..1] + 'ay'
+    elsif input[0..2] =~ /[^aeiouAEIOU]/ && input[3] =~ /[aeiouAEIOU]/
+      return input[3..-1] + input[0..2] + 'ay'
     else
-      return input[0] + input + 'ay'
+
     end
   end
 end
