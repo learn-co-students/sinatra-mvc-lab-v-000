6c6e6003e056e147af02a9b6591aba78c0159bed
Leslie Stevens <flylady@mail.utexas.edu>
2018-10-03 21:12:14 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,11 +4,15 @@ class PigLatinizer
   attr_reader :text
 
   def initialize
-    @text = @phrase
+    @text = text
   end
 
   def latinizer
     array = @text.split(//)
+    if [aeiou].include?(array[0])
+      array.concat(["w", "a", "y"])
+      array
+    end
   end
 
   def count_of_vowels
