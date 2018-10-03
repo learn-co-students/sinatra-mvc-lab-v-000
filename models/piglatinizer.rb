e9cfa0767aa057253d773b56b8a914bd36699b83
Leslie Stevens <flylady@mail.utexas.edu>
2018-10-03 19:46:37 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class PigLatinizer
   attr_reader :text
 
@@ -5,9 +7,8 @@ class PigLatinizer
     @text = text.downcase
   end
 
-  def count_of_words
-    words = text.split(" ")
-    words.count
+  def latinizer
+    array = @text.split(//)
   end
 
   def count_of_vowels
@@ -18,25 +19,5 @@ class PigLatinizer
     text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
 
-  def most_used_letter
-    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
-    arr = s1.split('')
-    arr1 = arr.uniq
-    arr2 = {}
 
-    arr1.map do |c|
-      arr2[c] =  arr.count(c)
-    end
-
-    biggest = { arr2.keys.first => arr2.values.first }
-
-    arr2.each do |key, value|
-      if value > biggest.values.first
-        biggest = {}
-        biggest[key] = value
-      end
-    end
-
-    biggest
-  end
 end
