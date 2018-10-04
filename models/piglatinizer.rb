fbce503c797601fc63737189b3460f3445cee955
Leslie Stevens <flylady@mail.utexas.edu>
2018-10-04 23:18:19 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,18 +1,39 @@
 require 'pry'
 
 class PigLatinizer
-  attr_accessor :phrase
+  attr_accessor :phrase, :string_array
+
+  def consonant(string_array)
+    string_array.concat(["#{string_array[0]}"])
+    string_array.shift
+    string_array
+  end
 
 
 
   def piglatinize(input_string)
-    string_array = input_string.split(//)
+    @string_array = input_string.split(//)
     vowel_array = ["a", "e", "i", "o", "u"]
-    if vowel_array.include?(string_array[0])
+
+    if vowel_array.include?(string_array[0]) == false
+      self.consonant(string_array)
+      #letter = string_array[0]
+      #string_array.concat(["#{letter}"])
+      #string_array.shift
+      #string_array.piglatinize
+
+    elsif vowel_array.include?(string_array[0])
       string_array.concat(["w", "a", "y"])
       string_array.join()
-      
+
+
     end
+  end
+
+  def consonant(string_array)
+    string_array.concat(["#{string_array[0]}"])
+    string_array.shift
+    string_array
   end
 
   def count_of_vowels
