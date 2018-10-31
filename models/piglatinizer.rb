32104d7158f7ab7185874d76a400b0169a81baf1
Barbara Choy <barbchoy@gmail.com>
2018-10-31 03:18:46 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,7 +4,21 @@ class PigLatinizer
     @orig_string = string
   end
 
-  def piglatinize_word
-    @orig_string[1..-1] + @orig_string[0] + "ay"
+  def piglatinize_word(word)
+    if start_with_vowel?(word)
+      @orig_string + "ay"
+    else
+      @orig_string[1..-1] + @orig_string[0] + "ay"
+    end
+  end
+
+  def piglatinize
+    words = @orig_string.split(" ")
+    words.map {|w| w.piglatinize_word(w)}
+    words.join(" ")
+  end
+
+  def start_with_vowel?(word)
+    "AaEeIiOoUu".split("").include?(word[0])
   end
 end
