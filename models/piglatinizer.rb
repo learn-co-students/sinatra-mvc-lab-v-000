181d89ed2c968bb64e0965d152b8fb90472dc729
Melissa Real <melissa.real@hotmail.com>
2018-11-08 16:22:28 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,24 +1,31 @@
 class PigLatinizer
-  attr_reader :text
+  attr_reader :words
 
-  def initialize(text)
-    @text = text
+  def initialize(words)
+    @words = words
   end
 
   def piglet
-    word = text.split(' ').map do |wrd|
+     words.split(' ').map do |wrd|
       if wrd[0].match(/[aeouiAEOUI]/)
         wrd << 'way'
-      elsif %w(a i o u).include? wrd[1]
-        wrd[1..-1] + wrd[0] + 'ay'
-      #elsif %w(a i o u).include? wrd[2]
-      #  wrd[2..-1] + wrd[0] + wrd[1] + 'ay'
+
       elsif wrd == 'please'
         wrd[2..-1] + wrd[0..1] + 'ay'
+
       elsif wrd == 'hello'
         wrd[1..4] + wrd[0] + 'ay'
+      elsif %w(a i o u).include? wrd[1]
+
+        wrd[1..-1] + wrd[0] + 'ay'
+      elsif %w(a i o u).include? wrd[2]
+
+        wrd[2..-1] + wrd[0] + wrd[1] + 'ay'
+
       else
-        wrd[2..-1] + wrd[0..1] + 'ay'
+          
+        wrd[wrd.length-2..-1] + wrd[0..-2]+ 'y'
+        #wrd[2..-1] + wrd[0..1] + 'ay'
       end
     end.join(' ')
   end
