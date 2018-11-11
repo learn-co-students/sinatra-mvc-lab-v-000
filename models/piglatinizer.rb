5a6c4885b480fb109cea08efb5a7b3728b9e69e7
Melissa Real <melissa.real@hotmail.com>
2018-11-11 23:10:35 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,51 +1,47 @@
 class PigLatinizer
   attr_reader :words
 
-  def initialize(words)
+  def initialize(words=nil)
     @words = words
   end
 
   def piglet
 
-  alpha = ('a'..'z').to_a
-  vowels = %w[a e i o u]
-  consonants = alpha - vowels
-  alpha = ('a'..'z').to_a
-  vowels = %w[a e i o u]
-  consonants = alpha - vowels
+  #alpha = ('a'..'z').to_a
+  #vowels = %w[a e i o u]
+  consonants = /\A(?=[^aeiou])(?=[a-z])/i
+  vowels = %w[a e i o u A E I O U]
+ #each word:
 
-  if words[0].match(/[aeouiAEOUI]/)
-    words + 'way'
-  elsif vowels.include?(words[0])
-    words + 'ay'
-  elsif consonants.include?(words[0]) && consonants.include?(words[1])
-    words[2..-1] + words[0..1] + 'ay'
-  else consonants.include?(words[0])
-    words[1..-1] + words[0] + 'ay'
-  end
+  # if vowels.include?(words[0])
+  #   words + 'way'
+  # elsif consonants.match(words[0]) && consonants.match(words[1]) && consonants.match(words[2])
+  #   words[3..-1] + words[0..2] + 'ay'
+  # elsif consonants.match(words[0]) && consonants.match(words[1])
+  #   words[2..-1] + words[0..1] + 'ay'
+  # else consonants.match(words[0])
+  #   words[1..-1] + words[0] + 'ay'
+  # end
 
-      #  words.split(' ').map do |wrd|
-      #   if wrd[0].match(/[aeouiAEOUI]/)
-      #     wrd << 'way'
-      #   elsif wrd == 'please'
-      #     wrd[2..-1] + wrd[0..1] + 'ay'
-      #   elsif wrd == 'hello'
-      #     wrd[1..4] + wrd[0] + 'ay'
-      #   elsif %w(a i o u).include? wrd[1]
-      #     wrd[1..-1] + wrd[0] + 'ay'
-      #   elsif %w(a i o u).include? wrd[2]
-      #     wrd[2..-1] + wrd[0] + wrd[1] + 'ay'
-      #   else
-      #     wrd[wrd.length-2..-1] + wrd[0..-2]+ 'y'
-      #
-      #   end
-      # end.join(' ')
+ #everything:
+    words.split(' ').map do |wrd|
+      if vowels.include?(wrd[0])
+        wrd << 'way'
+      elsif consonants.match(wrd[0]) && consonants.match(wrd[1]) && consonants.match(wrd[2])
+        wrd[3..-1] + wrd[0..2] + 'ay'
+      elsif consonants.match(wrd[0]) && consonants.match(wrd[1])
+        wrd[2..-1] + wrd[0..1] + 'ay'
+      else consonants.match(wrd[0])
+        wrd[1..-1] + wrd[0] + 'ay'
+      end
+    end.join(' ')
+
   end
 
   def piglet_phrase(a)
-      a.each do |word|
+      a.split(' ').each do |word|
         word.piglet
-      end
+      end.join(' ')
   end
 
 
