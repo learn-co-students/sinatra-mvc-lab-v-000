a7106bd6c885115a4a00b6a5f4022f5fa82c31da
Harley Rayner <harleymteden@gmail.com>
2018-11-24 16:59:19 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+class PigLatinizer
+  attr_accessor :user_text
+
+  def initialize(text)
+    @user_text = text
+  end
+
+  def piglatinize(word)
+    if word =~ (/\A[aeiou]/i)
+      word = word + 'ay'
+    elsif word =~ (/\A[^aeiou]/i)
+      match = /\A[^aeiou]/i.match(word)
+      word = match.post_match + match.to_s + 'ay'
+    end
+    word
+  end
+
+
+end
