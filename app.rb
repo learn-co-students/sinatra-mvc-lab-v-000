8d376a52bf1803b3987d956cd32c19d6b6e95e0a
Rima Jean <rima.jean@gmail.com>
2018-11-27 16:16:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    binding.pry
+    # binding.pry
     @text =  PigLatinizer.new.to_pig_latin(params[:user_phrase])
     erb :results
   end
