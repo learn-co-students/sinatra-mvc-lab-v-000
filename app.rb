fde7a71efb014aefcd27e8397962cb0b903e8a6e
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-12-17 23:17:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    status 200
+
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    status 200
+    @results = PigLatinizer.new
+
+    erb :results
+  end
 end
