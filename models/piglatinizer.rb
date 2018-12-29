7980821280f90f1cdb858cf90fd6d34e5aa22d7a
Rafael Lopez <lpz.raf@gmail.com>
2018-12-29 18:40:23 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,18 +2,21 @@
 class PigLatinizer
   
   def piglatinize(word) 
-    not_piglatinizable = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
-    vowels = ["a", "e", "i", "o", "u"]
+    #not_piglatinizable = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     after_vowel = []
+    
     if vowels.include?(word[0])
       word + "way"
     else
       until vowels.include?(word[0])
         after_vowel << word[0]
         word[0] = ""
-        
-    
-
+      end
+      word.split('')
+      word = word + after_vowel.join("")
+      word + "ay"
+    end
   end
   
   def to_pig_latin(sentence)
