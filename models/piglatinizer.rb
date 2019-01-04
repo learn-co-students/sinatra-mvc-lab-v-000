cc8be994ff3eca698d130fac0109dbd6e04278a2
Sharmaine Requerme <srqrme@gmail.com>
2019-01-04 04:45:26 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,8 +4,18 @@ class PigLatinizer
 
   def piglatinize(word)
     word_array = word.split("")
+    if word_array[0] == "a" || word_array[0] == "e" || word_array[0] == "i" || word_array[0] == "o" || word_array[0] == "u"
+      word_array << word_array.shift
+    end
+    word_array << "ay"
+    word_array.join
   end
   binding.pry
+
+  def vowel?(letter)
+    letter.scan(/[aAeEiIoOuU]/)
+  end
+
 end
 
   PigLatinizer.new.piglatinize('apple')
