bf6b9ef7476e6700e2fe6855efe3310d5c43c98d
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-07 16:58:26 -0500

    Add app controller code

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/translation' do
+    @text_input = PigLatinizer.new(params[:user_input])
+
+    erb :translation
+  end
+
 end
