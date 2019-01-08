fb422797bdfd0078856b793eefb53fdbfe04be46
Vince Andrew Pimentel <contact@vincep.me>
2019-01-08 10:30:01 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,23 +1,23 @@
 class PigLatinizer
-  attr_reader :words
-
-  def initialize(words)
-    @words = words
+  def piglatinize(string)
+    string.include?(" ") ? piglatinize_sentence(string) : piglatinize_word(string)
   end
 
-  def piglatinize
-    words_array = words.split(" ")
+  def piglatinize_word(word)
+    if word.scan(/\A[^aeiouAEIOU]+/).size != 0
+      consonants = word.scan(/\A[^aeiouAEIOU]+/)
+      letters = consonants.first
+      size = letters.length
 
-    words_array.collect do |word|
-      if word.scan(/\A[^aeiouAEIOU]+/).size != 0
-        consonants = word.scan(/\A[^aeiou]+/)
-        letters = consonants.first
-        size = letters.length
+      "#{word[size..-1]}#{letters}ay"
+    else
+      "#{word}way"
+    end
+  end
 
-        "#{word[size..-1]}#{letters}ay"
-      else
-        "#{word}way"
-      end
+  def piglatinize_sentence(sentence)
+    sentence.split(" ").collect do |word|
+      piglatinize_word(word)
     end.join(" ")
   end
 end
