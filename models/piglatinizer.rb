c08142a80f530c650a07c88380976b0864f877ac
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-12 18:25:45 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,28 +3,34 @@ require 'pry'
 class PigLatinizer
   attr_accessor :phrase, :string_array
 
+  def word_array(word)
+    word_array = word.split("")
+    word_array.piglatinize
+  end
+
 
 
   def piglatinize(word)
     word_array = word.split("")
+
     length = word_array.count
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     result = []
     letter = word_array[0]
     if !vowels.include?(letter)
       word_array = word_array[1..length-1].push(letter)
-      piglatinize(word_array.join(""))
+      word_joined = word_array.join("")
+      piglatinize(word_joined)
+      binding.pry
     else
+
       word_array.push("w")
     end
     result = word_array
     #binding.pry
     result.concat(["a", "y"])
-    #binding.pry
-    result.join("")
-    #return result
-    #binding.pry
+
+
+    return result.join("")
   end
-
-
 end
