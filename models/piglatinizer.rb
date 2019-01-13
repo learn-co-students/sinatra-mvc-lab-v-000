f5c07f6b8a15848da3cee166c44cd47776e4c0fd
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-13 23:15:54 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,52 +8,27 @@ class PigLatinizer
 
   def piglatinize(word)
     word_array = word.split("")
-    #binding.pry
     length = word_array.count
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     result = []
     letter = word_array[0]
-
-    if !vowels.include?(letter)
-      #binding.pry
-      starts_with_consonant(word_array)
-    elsif vowels.include?(letter) && word_array != nil
-      #binding.pry
+    if vowels.include?(letter) || length == 1
       result = word_array.concat(["w", "a", "y"])
     end
-
-    result.join("")
-
-  end
-
-
-
-  def starts_with_consonant(word_array)
-    #binding.pry
-    length = word_array.count
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    result = []
-    letter = word_array[0]
-    if length == 1
-      result = word_array.concat(["w", "a", "y"])
-      return result.join("")
-    end
-    if !vowels.include?(letter)
+    if !vowels.include?(letter) && word_array != nil
       word_array = word_array[1..length-1].push(letter)
-      #binding.pry
-      starts_with_consonant(word_array)
-    else
-      new_result = word_array
+      letter = word_array[0]
+      if !vowels.include?(letter)
+        word_array = word_array[1..length-1].push(letter)
+        letter = word_array[0]
+        if !vowels.include?(letter)
+          word_array = word_array[1..length-1].push(letter)
+          letter = word_array[0]
+        end
+      end
+      result = word_array.concat(["a", "y"])
     end
-    if new_result != nil
-      new_result_concat = new_result.concat(["a", "y"])
-    end
-    if new_result_concat != nil
-      new_result_join = new_result_concat.join("")
-    end
-
-
-    new_result_join
-
+    result.join
   end
+
 end
