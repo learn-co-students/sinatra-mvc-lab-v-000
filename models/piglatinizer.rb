b636da566f88992686a14fd8b139809cbf9b4572
Heidi Hlavinka <hchlav@gmail.com>
2019-01-27 20:33:11 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -19,8 +19,19 @@ class PigLatinizer
     consonant = text.scan(/[bcdfghjklmnpqrstvwxyz]/)
     vowel = text.scan(/[aeoui]/)
 
-    # if word.start_with?(consonant)
+    if word.start_with?(consonant)
+
+      # if word starts with 1 consonant
+        # take 1 consonant, move to end of string, add ay after
+      # if word starts with 1+ consonant
+        #take all consonants, move to end of string, add ay after
+      # if word starts with 1 vowel
+        # add ay to end of word
 
   end
 
 end
+
+# identify words start with consonants and move consonants to end of string
+# add ay to end of string after consonants
+# identify words start with vowels and ay to end of those strings
