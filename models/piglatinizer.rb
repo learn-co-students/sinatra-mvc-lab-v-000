e3002f841497c9f298aab5e06f75a181d52c49fa
Heidi Hlavinka <hchlav@gmail.com>
2019-01-31 02:11:55 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,17 +8,18 @@ class PigLatinizer
     end
   end
 
-  def piglatinize_word(word)
-    consonant = word.scan(/[bcdfghjklmnpqrstvwxyz]/)
-    vowel = word.scan(/[aeoui]/)
-    #  binding.pry
-    if word.start_with?(consonant) && consonant == 1
-      word << word[0] + "ay"
-    elsif word.start_with?(consonant) && consonant == 2
+  def piglatinize_word(word) #<< "pork"
+    consonant = word.match(/[bcdfghjklmnpqrstvwxyz]/)  #scan returned array of strings
+    vowel = word.match(/[aeoui]/)
+    if word.start_with?(consonant[0])
+      word[1..3] + word[0] + "ay"
+      binding.pry
+
+    elsif word.start_with?(consonant[0]) && consonant[1]
       word << word[0..1] + "ay"
-    elsif word.start_with?(consonant) && consonant == 3
+    elsif word.start_with?(consonant[0]) && consonant[1] && consonant [2]
       word << word[0..2] + "ay"
-    else word.start_with?(vowel)
+    else word.start_with?(vowel[0])
       word << "ay"
     end
       # if word starts with 1 consonant
