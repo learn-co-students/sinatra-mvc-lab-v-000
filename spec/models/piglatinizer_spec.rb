5af52ee2c7b4791e3e98930e2096c7df2c7423e7
James Randolph <james.randolph2016@gmail.com>
2019-02-06 22:12:09 +0000

    Done.

--- spec/models/piglatinizer_spec.rb
+++ spec/models/piglatinizer_spec.rb
@@ -19,8 +19,6 @@ describe 'class PigLatinizer' do
     expect(words.piglatinize("prays")).to eq("ayspray")
   end
 
-  it 'has a method splits the sentence to piglatinize each word' do
-    expect(words.to_pig_latin("i love programming")).to eq("iway ovelay ogrammingpray") 
-  end
+
   
 end
