aa0901f7f2c325e5cd521d8a04f419e0e2efdc62
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-02-20 11:54:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get "/" do
+        erb :user_input
+    end
+    post "/piglatinize" do
+        "Your new phrase is '#{params[:user_phrase]}'"
+    end
+
 end
