3bb02610b00742ddc5e6873be57e199c8ad6ae2b
Ashley Wong <ashwong04@gmail.com>
2019-02-27 20:34:32 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,18 +3,27 @@ require 'pry'
 class PigLatinizer
   
   def piglatinize(word)
-      pl = word.split('')
-      until pl[0].match(/[aeiouAEIOU]/)
-        pl = pl.insert(-1, pl.delete_at(0))
-      end
+    arr = []
+    if word.size == 1
+      arr << word
+    else
+      arr = word.split(' ')
+      binding.pry
+    end
+
+    pl = word.split('')
+    until pl[0].match(/[aeiouAEIOU]/)
+       pl = pl.insert(-1, pl.delete_at(0))
+    end
       
-      new_word = pl.join
+    new_word = pl.join
       
-      if word.split('')[0].match(/[aeiouAEIOU]/)
-        new_word << "way"
-      else
-        new_word << "ay"
-      end
+    if word.split('')[0].match(/[aeiouAEIOU]/)
+      new_word << "way"
+    else
+      new_word << "ay"
+    end
+    
   end
   
   
