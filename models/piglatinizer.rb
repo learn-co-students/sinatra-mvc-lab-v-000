a0fddabcd30e8325ecfb718ec86ceb2ba2b63b4e
Ashley Wong <ashwong04@gmail.com>
2019-02-27 20:50:25 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,24 +4,22 @@ class PigLatinizer
   
   def piglatinize(word)
     arr = word.split(' ')
-   
-      # binding.pry
-    latin = arr.each do |w|
+    arr.each do |w|
       pl = w.split('')
       until pl[0].match(/[aeiouAEIOU]/)
          pl = pl.insert(-1, pl.delete_at(0))
       end
         
       new_word = pl.join
-        
       if w.split('')[0].match(/[aeiouAEIOU]/)
         new_word << "way"
       else
         new_word << "ay"
       end
     end
-    latin
   end
+  
+  
   
   
 end
