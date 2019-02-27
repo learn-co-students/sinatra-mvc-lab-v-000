d75d63df364ddfbcd90f4e435ae8fe981d5ecc76
Ashley Wong <ashwong04@gmail.com>
2019-02-27 20:25:23 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,12 +5,12 @@ class PigLatinizer
   def piglatinize(word)
       pl = word.split('')
       until pl[0].match(/[aeiouAEIOU]/)
-          pl = pl.insert(-1, pl.delete_at(0))
-        end
+        pl = pl.insert(-1, pl.delete_at(0))
+      end
       
       new_word = pl.join
       
-      if new_word.end_with?("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
+      if pl.last.match(/[aeiouAEIOU]/)
         new_word << "way"
       else
         new_word << "ay"
