5c1d9ca9d79fbdfda80361577498da4aab74d8bc
Cheryl K <cherylkelson@icloud.com>
2019-03-11 13:42:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/piglatinizer' do
+  post '/piglatinize' do
     @analyzed_word = WordAnalyzer.new(params[:user_word])
     erb :results
   end
