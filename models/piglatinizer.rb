075c435ac0329b6ed1664ee3c258ed882f489c1a
Laura Messa Travin <laura.travin@gmail.com>
2019-03-26 19:17:22 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -15,7 +15,7 @@ class PigLatinizer
     #recorrer el array y cambiar segun las reglas
     data2 =  data.map {|e|
 
-          if  ( VOWELS.include?(e[0]) && !VOWELS.include?(e[1]))||(VOWELS.include?(e[0]) && VOWELS.include?(e[1])) 
+          if   VOWELS.include?(e[0]) #&& !VOWELS.include?(e[1]))||(VOWELS.include?(e[0]) && VOWELS.include?(e[1])) 
                e << "way"
           elsif !VOWELS.include?(e[1]) && !VOWELS.include?(e[2])
                e= e[3..-1]+ e[0..2] +"ay"                     #spray
