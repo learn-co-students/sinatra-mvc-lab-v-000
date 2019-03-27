f43ddf5c0ed8ee4a75310df3410838a80cf9aa1d
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-27 15:05:57 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+class PigLatinizer
+    
+    def translate(str)
+        if str.downcase.start_with?("a", "e", "i", "o", "u")
+            str << "way"
+        else
+            o_str = str[0, %w(a e i o u).map{|vowel| "#{str}aeiou".index(vowel)}.min]
+            result = "#{str[o_str.length..-1]}#{o_str}ay"
+
+            result
+           binding.pry
+        end
+    end
+
+    def piglatinize(str)
+		str.split(" ").map{|word| translate(word)}.join(" ")
+	end
+
+end
