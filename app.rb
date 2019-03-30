c3b70ea1ccaeb004ecc04991983b6c2f61c3bbe5
Sheena Bonilla <sheena.myb@gmail.com>
2019-03-30 19:07:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
-  
-  post '/piglatinize' do
 
+  post '/piglatinize' do
+    erb :piglatinize
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 class PigLatinizer
-  
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def piglatinize
+    binding.pry
+    @words.split("")
+  end
 end
