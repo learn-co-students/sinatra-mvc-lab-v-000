a48f7b92502860e9ab0b6a152ac6c0aca8206db0
Thomas Gray <tgray017@gmail.com>
2019-04-09 12:33:55 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,38 +1,37 @@
 class PigLatinizer
-  attr_reader :content
-  
+
   def piglatinize(input)
-    parse_input(input)
-    input_array = input.split('')
-    ending = input_array.first.split('').push("a").push("y")
-    beginning = input_array[1..-1]
-    (beginning + ending).join
+    @output = []
+    parse_sentence(input)
+    @output.join(' ')
   end
   
-  def parse_input(input)
-    #binding.pry
+  def parse_sentence(input)
+    input_array = input.split(' ')
+    input_array.each {|word| parse_word(word)}
+  end
+  
+  def parse_word(input)
     input_array = input.split('')
     
     first_letters = []
     last_letters = []
     
     input_array.each do |letter|
-      case
-        when !['a','e','i','o','u'].include?(letter) && first_letters.empty?
-          last_letters << letter
-        else
-          first_letters << letter
+      if !['a','e','i','o','u'].include?(letter.downcase) && first_letters.empty?
+        last_letters << letter
+      else
+        first_letters << letter
       end
     end
     
-    if ['a','e','i','o','u'].include?(input_array.first)
-      word = (first_letters << last_letters << "way").join
+    if ['a','e','i','o','u'].include?(input_array.first.downcase)
+      pl_word = (first_letters << last_letters << "way").join
     else
-      word = (first_letters << last_letters << "ay").join
+      pl_word = (first_letters << last_letters << "ay").join
     end
     
-    
-    binding.pry
+    @output << pl_word
   end
   
 end
