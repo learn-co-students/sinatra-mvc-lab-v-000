df656dffd9130c408fdcbd03646b25515bb901c9
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-10 20:19:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  # create get route in '/' to display the form user_input
+  get '/' do
+    erb :user_input
+  end
+
+  # create post route in /piglatinize to create an instance of piglatinizer and
+  # @piglatinize = PigLatinizer.new(params[:user_phrase])
+  #
+  # display the results of the piglatinization
+  # erb :piglatinized
+
+  post '/piglatinize' do
+    piglatin = PigLatinizer.new
+    @results = piglatin.piglatinize(params[:user_phrase])
+    erb :piglatinized
+  end
 end
