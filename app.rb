265a8debe18bad633c12b4a3b37b1e0f662dffb8
Paul Fell <swellpf@gmail.com>
2019-06-07 19:26:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+
+    erb :index
+  end
+
+  post '/' do
+    @latinized_text = PigLatinizer.new(params[:user_text])
+    erb :results
+  end
 end
