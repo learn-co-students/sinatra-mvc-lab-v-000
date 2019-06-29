51e79dc0414e0b925a79fda92e9378004ac55ef7
Steven Crouse <sdcrouse91@gmail.com>
2019-06-29 01:57:59 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+class PigLatinizer
+    # "this" should become "isthay"
+    # "is" should become "isway"
+    # "HearT" should become "earthay"
+    
+    # I need to first remove extra characters, whitespace, numbers, etc.
+    # Then, convert it all to downcase (to make things easier)
+    # Then, isolate each word.
+    # Convert each word to Pig Latin (see examples above).
+    # Reassemble the words into a phrase, and return the phrase.
+    
+  def piglatinize(phrase)
+    
+  end
+end
