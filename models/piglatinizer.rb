4e996388d6ba95dff92aa97564352dd294bba484
Steven Crouse <sdcrouse91@gmail.com>
2019-06-29 03:24:41 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,12 +10,31 @@ class PigLatinizer
     # Reassemble the words into a phrase, and return the phrase.
     
   def piglatinize(phrase)
-    letters_only(phrase).downcase
+    # "Th4is is a Phr%%ase)" becomes ["This", "is", "a", "Phrase"]
+    phrase_words = letters_only(phrase).split
+    
+    # phrase_words.collect do |word|
+      # word.downcase (This was a test of the return value.)
+      # if (word starts with a vowel)
+        # add "way" to the end of the word
+      # else # word starts with a consonant
+        # Take all of the starting consonants of a word up to the first vowel.
+        # Move those consonants to the end of the word to make a new string.
+        # Add "ay" to the end of that new string.
+      # end
+    # end.join(" ")
+    
+    phrase_words.collect do |word|
+      
+    end.join(" ")
   end
   
   private
   
-  def letters_only(phrase)
-    phrase.gsub(/[^a-zA-Z]/, "")
+  def letters_only(phrase) # Edge case
+    # Remove everything from the phrase except letters and whitespace characters.
+    # "Th4is is a Phr%%ase)" becomes "This is a Phrase"
+    
+    phrase.gsub(/[^a-zA-Z\s]/, "")
   end
 end
