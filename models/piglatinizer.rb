5621704f845fc9790ba1880e0d8cabc25f68d4e7
Steven Crouse <sdcrouse91@gmail.com>
2019-06-29 03:58:02 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,6 +9,8 @@ class PigLatinizer
     # Convert each word to Pig Latin (see examples above).
     # Reassemble the words into a phrase, and return the phrase.
     
+    # Interesting: a MatchData object (returned from the #match method) recognizes [0] but not #first.
+    
   def piglatinize(phrase)
     # "Th4is is a Phr%%ase)" becomes ["This", "is", "a", "Phrase"]
     phrase_words = letters_only(phrase).split
@@ -20,17 +22,19 @@ class PigLatinizer
       # else # word starts with a consonant
         # Take all of the starting consonants of a word up to the first vowel.
         # Move those consonants to the end of the word to make a new string.
-        # Add "ay" to the end of that new string.
+        # Add "ay" to the end of that new string. Maybe use #append here and above.
       # end
     # end.join(" ")
     
     phrase_words.collect do |word|
-      if word.start_with?(/[aeiouAEIOU]/)
+      if word.start_with?(/[aeiouAEIOU]/) # "either" becomes "eitherway"
         "#{word}way"
-      else
-        
+      else # "fire" becomes "irefay" and "smith" becomes "ithsmay"
+        first_consonants = word.match(/[BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz]+/)
+        # That won't work the way I want it to. If the word is "testing" and I split it at "t", then I'll get ["", "es", "ing"] instead of ["esting"].
       end
     end.join(" ")
+    
   end
   
   private
