d74ac511f9030980e52e00fdf8ff98a35719a0ff
Karly Mareka <karly.mareka@gmail.com>
2019-07-04 03:43:03 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -38,30 +38,4 @@ class PigLatinizer
     end 
     
   end 
-=begin  
-  def piglatinize_word(word)
-      if ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"].include?(word[0])
-        @result = "#{word}way"
-      elsif ["spr", "str"].any? {|consonant_blend| word[0..2].include? consonant_blend}
-        word = word.split("")
-        first_letter = word.shift
-        second_letter = word.shift
-        third_letter = word.shift 
-        word = word.join
-        @result = "#{word}#{first_letter}#{second_letter}#{third_letter}ay"
-      elsif ["bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"].any? {|consonant_blend| word[0..1].include? consonant_blend}
-        word = word.split("")
-        first_letter = word.shift
-        second_letter = word.shift 
-        word = word.join
-        @result = "#{word}#{first_letter}#{second_letter}ay"
-      else
-        word = word.split("")
-        first_letter = word.shift
-        word = word.join 
-        @result = "#{word}#{first_letter}ay"
-      end
-    @result
-  end 
-=end  
 end 
