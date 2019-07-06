ffc8129222bcb3b136a4707ed40a88d52c2d7d73
jamieberrier <berrier.jamie@gmail.com>
2019-07-06 03:13:12 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,17 @@
 class PigLatinizer
   attr_reader :text
 
-  def initialize
-
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
   end
 
   def piglatinize(text)
     words = text.split(" ")
 
     words.collect! do |word|
-      if word.start_with?(/[aeouiAEOUI]/)
-        word << 'way'
-      elsif word.start_with?('sch', 'scr', 'shr', 'spl', 'spr', 'squ', 'str', 'thr', 'Sch', 'Scr', 'Shr', 'Spl', 'Spr', 'Squ', 'Str', 'Thr')
+      if !consonant?(word[0])
+        word << 'w'
+      elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
         array = word.chars
 
         first_letter = array[0]
@@ -22,8 +22,8 @@ class PigLatinizer
         array.delete_at(0)
         array.delete_at(0)
 
-        word = array.join + first_letter + second_letter + third_letter + 'ay'
-      elsif word.start_with?('bl', 'br', 'ch', 'ck', 'cl', 'cr', 'dr', 'fl', 'fr', 'gh', 'gl', 'gr', 'ng', 'ph', 'pl', 'pr', 'qu', 'sc', 'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 'st', 'sw', 'th', 'tr', 'tw', 'wh', 'wr', 'Bl', 'Br', 'Ch', 'Ck', 'Cl', 'Cr', 'Dr', 'Fl', 'Fr', 'Gh', 'Gl', 'Gr', 'Ng', 'Ph', 'Pl', 'Pr', 'Qu', 'Sc', 'Sh', 'Sk', 'Sl', 'Sm', 'Sn', 'Sp', 'St', 'Sw', 'Th', 'Tr', 'Tw', 'Wh', 'Wr')
+        word = array.join + first_letter + second_letter + third_letter
+      elsif consonant?(word[0]) && consonant?(word[1])
         array = word.chars
 
         first_letter = array[0]
@@ -32,16 +32,16 @@ class PigLatinizer
         array.delete_at(0)
         array.delete_at(0)
 
-        word = array.join + first_letter + second_letter + 'ay'
-      elsif word.start_with?(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)
+        word = array.join + first_letter + second_letter
+      else
         first_letter = word.chr
-        ending = first_letter + 'ay'
 
         array = word.chars
         array.delete_at(0)
 
-        word = array.join + ending
+        word = array.join + first_letter
       end
+      word << 'ay'
     end
 
     "#{words.join(" ")}"
