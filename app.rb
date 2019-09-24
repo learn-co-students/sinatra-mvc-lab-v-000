5af5e7ea485b48e6672ffb8b97e6abf651a84641
Chao Hsuan Chi <allen0413@gmail.com>
2019-09-24 21:20:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
-require_relative 'config/environment'
+require_relative 'config/environment.rb'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    text_from_user = params[:user_text]
+
+    erb :results
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,27 @@
+class PigLatinizer
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+  end
+
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
+
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
+  end
+
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+    no_spaces = text.gsub(" ", "")
+    letters = no_spaces.split("")
+    histogram = Hash[*letters.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+    histogram.max_by{ |k, v| v }
+  end
+end
