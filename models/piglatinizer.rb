5169a022f4837ef014a02c315c313a5dfda80426
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-27 23:08:22 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,34 +1,67 @@
 
 class PigLatinizer
-    def piglatinize(word)
+    def piglatinize(words)
+      # we are going to create a method that we can use here that will piglatinize an individual word
+      
+      # separate the words from the array and create a new array
+       words.split(" ").collect do |word|
+      # use our method piglatinize_word
+        piglatinize_word(word)
+      # join
+       end.join(" ")
+    end
 
-      consonant = /\A[^AEIOUaeiou]+/.match(word)
-      remainder = /[AEIOUaeiou]+\w+/.match(word) 
-      vowel = /\A[AEIOUaeiou]/.match(word)
-      whole_word = /\A\w*/.match(word)
-  
-      if consonant
-        new_word = remainder[0] << consonant[0] << "ay"
-      elsif vowel
-        new_word = whole_word[0] << "way"
-      end  
-    end
-    
-    def splits(sentence)
-      sentence.piglatinize(word)
-    end
+def piglatinize_word(word)
+
+  if word[0].match(/[aeiouAEIOU]/)
+    word + "way"
+  elsif !word[0].match(/[aeiouAEIOU]/) && word[1].match(/[aeiouAEIOU]/)
+    first_letter = word.slice!(0)
+    word + first_letter + "ay"
+
+  elsif word[2].match(/[aeiouAEIOU]/)
+    first_two_letters = word.slice!(0, 2)
+    word + first_two_letters + "ay"
+  elsif word[3].match(/[aeiouAEIOU]/)
+    first_three_letters = word.slice!(0, 3)
+    word + first_three_letters + "ay"
+  end
+end
 end
 
 
 
 
 # class PigLatinizer
-#   def piglatinize(word)
-    
+#   def piglatinize(words)
+#     words_array = words.split(" ")
+#     results_array = []
+
+#     words_array.each do |word|
 #     consonant = /\A[^AEIOUaeiou]+/.match(word)
 #     remainder = /[AEIOUaeiou]+\w+/.match(word) 
 #     vowel = /\A[AEIOUaeiou]/.match(word)
 #     whole_word = /\A\w*/.match(word)
+
+#     if consonant
+#       new_word = remainder[0] << consonant[0] << "ay"
+#       puts new_word
+#     elsif vowel
+#       new_word = whole_word[0] << "way"
+#     end
+#     end
+    
+#   end
+# end
+
+
+# class PigLatinizer
+#   def piglatinize(words)
+    
+#     consonant = /\A[^AEIOUaeiou]+/.match(words)
+#     remainder = /[AEIOUaeiou]+\w+/.match(words) 
+#     vowel = /\A[AEIOUaeiou]/.match(words)
+#     whole_word = /\A\w*/.match(words)
 
 #     if consonant
 #       new_word = remainder[0] << consonant[0] << "ay"
@@ -39,9 +72,66 @@ end
 # end
 
 
+
+
+
 # consonant = word.match(/(\A[^AEIOUaeiou])/).captures
 # consonant[0]
 # remainder = word.match(/([AEIOUaeiou]+\w+)/)
 # remainder[0]
 # consonant = /(\A[^AEIOUaeiou])/.match(word)
 # remainder = /([AEIOUaeiou]+\w+)/.match(word)
+
+
+
+
+
+# class PigLatinizer
+#   def piglatinize(words)
+    
+#     words_array = words.split(" ")
+#     results_array = []
+    
+#     words_array.map do |word|
+#     #  puts word
+#       consonant = /\A[^AEIOUaeiou]+/.match(word)
+        
+#       # puts consonant
+#       remainder = /[AEIOUaeiou]+\w+/.match(word)
+#       # puts remainder
+#       vowel = /\A[AEIOUaeiou]/.match(word)
+      
+#       whole_word = /\A\w*/.match(word)
+      
+#       if consonant
+#        new_word = remainder[0] << consonant[0] << "ay"
+#       #  puts new_word
+        
+#       elsif vowel
+#         whole_word[0] << "way"
+        
+#       end     
+#       # results_array << new_word
+#     end
+#   end    
+# end
+
+ #   results_array = []
+      
+    #   words.split(" ").map do |word|
+    #     consonant = /\A[^AEIOUaeiou]+/.match(word)
+    #     remainder = /[AEIOUaeiou]+\w+/.match(word)
+    #     vowel = /\A[AEIOUaeiou]/.match(word)
+    #     whole_word = /\A\w*/.match(word)
+    #     if consonant
+    #      new_word = remainder[0] << consonant[0] << "ay" 
+    #      new_word.join(" ")
+    #     #  results_array << new_word
+    #     elsif vowel
+    #       new_word = whole_word[0] << "way"
+    #       results_array << new_word
+    #     end     
+    #     # results_array << new_word
+    #   end
+      
+    # end    
