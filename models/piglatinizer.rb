41d3c4527afffd421edb471dfbef63c8637f3339
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-27 23:17:46 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,33 +1,33 @@
 
-class PigLatinizer
-    def piglatinize(words)
-      # we are going to create a method that we can use here that will piglatinize an individual word
+# class PigLatinizer
+#     def piglatinize(words)
+#       # we are going to create a method that we can use here that will piglatinize an individual word
       
-      # separate the words from the array and create a new array
-       words.split(" ").collect do |word|
-      # use our method piglatinize_word
-        piglatinize_word(word)
-      # join
-       end.join(" ")
-    end
+#       # separate the words from the array and create a new array
+#        words.split(" ").collect do |word|
+#       # use our method piglatinize_word
+#         piglatinize_word(word)
+#       # join
+#        end.join(" ")
+#     end
 
-    def piglatinize_word(word)
+#     def piglatinize_word(word)
 
-      if word[0].match(/[aeiouAEIOU]/)
-        word + "way"
-      elsif !word[0].match(/[aeiouAEIOU]/) && word[1].match(/[aeiouAEIOU]/)
-        first_letter = word.slice!(0)
-        word + first_letter + "ay"
+#       if word[0].match(/[aeiouAEIOU]/)
+#         word + "way"
+#       elsif !word[0].match(/[aeiouAEIOU]/) && word[1].match(/[aeiouAEIOU]/)
+#         first_letter = word.slice!(0)
+#         word + first_letter + "ay"
 
-      elsif word[2].match(/[aeiouAEIOU]/)
-      first_two_letters = word.slice!(0, 2)
-      word + first_two_letters + "ay"
-      elsif word[3].match(/[aeiouAEIOU]/)
-      first_three_letters = word.slice!(0, 3)
-      word + first_three_letters + "ay"
-    end
-  end
-end
+#       elsif word[2].match(/[aeiouAEIOU]/)
+#       first_two_letters = word.slice!(0, 2)
+#       word + first_two_letters + "ay"
+#       elsif word[3].match(/[aeiouAEIOU]/)
+#       first_three_letters = word.slice!(0, 3)
+#       word + first_three_letters + "ay"
+#     end
+#   end
+# end
 
 
 class PigLatinizer
