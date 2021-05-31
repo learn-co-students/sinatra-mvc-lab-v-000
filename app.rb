a705188e6bae4cccd0fba17c4ea3ad2be75ab5ce
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-30 23:47:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,15 +6,11 @@ class App < Sinatra::Base
         erb :user_input
     end
 
-    # post '/' do
-    #   # @words = PigLatinizer.new(params[:user_text])
-    #   @words = PigLatinizer.new
-    #   erb :results
-    # end
+    post '/piglatinize' do
+      @phrase = PigLatinizer.new(params[:user_phrase])
+      erb :results
+    end
 
-    # post '/piglatinize' do
-    #   # @words = PigLatinizer.new(params[:user_text])
-    #   @words = PigLatinizer.new
-    #   erb :results
-    # end
+    
 end
+
