392733775486d38dc4f3c60858b22dcc9fc0fdae
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-31 22:57:04 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,39 +16,3 @@ class PigLatinizer
 end
 
 
-# class PigLatinizer
-#     def piglatinize(words)
-#       # we are going to create a method that we can use here that will piglatinize an individual word
-      
-#       # separate the words from the array and create a new array
-#        words.split(" ").collect do |word|
-#       # use our method piglatinize_word
-#         piglatinize_word(word)
-#       # join
-#        end.join(" ")
-#     end
-
-#     def piglatinize_word(word)
-
-#       if word[0].match(/[aeiouAEIOU]/)
-#         word + "way"
-#       elsif !word[0].match(/[aeiouAEIOU]/) && word[1].match(/[aeiouAEIOU]/)
-#         first_letter = word.slice!(0)
-#         word + first_letter + "ay"
-
-#       elsif word[2].match(/[aeiouAEIOU]/)
-#       first_two_letters = word.slice!(0, 2)
-#       word + first_two_letters + "ay"
-#       elsif word[3].match(/[aeiouAEIOU]/)
-#       first_three_letters = word.slice!(0, 3)
-#       word + first_three_letters + "ay"
-#     end
-#   end
-# end
-
-
-
-
-
-     
-    
